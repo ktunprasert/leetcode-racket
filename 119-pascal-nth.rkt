bf062e9297bdbcbfd0c9e4933e69427a11cd6859
ktunprasert <ktunprasert@outlook.com>
2025-06-04 23:53:44 +0100

    119-pascal-nth: oh yeah we cooked

--- 119-pascal-nth.rkt
+++ 119-pascal-nth.rkt
@@ -1,5 +1,30 @@
 #lang racket
 
-(define/contract (get-row rowIndex)
+;; size of list is n+1
+;; assume n = 3
+;; start with
+;; '(0 0 0 0)
+;; '(1 0 0 0) fixed return
+;; '(1 1 0 0) maybe start here - when n = 2 only care about (0, 1)
+;; '(1 2 1 0) n = 3; (0, 1, 2) -> permutation = ((0,1) (1,2))
+;; '(1 3 3 1) (0 1 2 3) -> ((0,1) (1,2) (2,3))
+(define/contract (get-row n)
   (-> exact-integer? (listof exact-integer?))
-  )
+  (match n
+    [1 '(1)]
+    [2 '(1 1)]
+    [_ (let loop ([i 2] [arr (vector->list (vector-extend #(1 1) (add1 n)))])
+         (cond
+           [(> i n) arr]
+           [else (loop
+                  (add1 i)
+                  (reverse
+                   (foldl append '(1)
+                          (build-list n
+                                      (lambda (x)
+                                        (list
+                                         (+
+                                          (list-ref arr x)
+                                          (list-ref arr (add1 x)))) )))) )]))]))
+
+(get-row 4)
