3b1e0fba1248b9d8e6745c2481d8e0483cd03849
ktunprasert <ktunprasert@outlook.com>
2025-06-04 20:39:26 +0100

    58-len-last-word: skip 's'

--- 58-len-last-word.rkt
+++ 58-len-last-word.rkt
@@ -1,9 +1,6 @@
 #lang racket
 
-(define/contract (length-of-last-word s)
-  (-> string? exact-integer?)
-  ((compose1 string-length last string-split) s)
-  )
+(define/contract length-of-last-word (-> string? exact-integer?) (compose1 string-length last string-split))
 
 (length-of-last-word "Hello World") ; should be 5
 (length-of-last-word "  a") ; should be 1
