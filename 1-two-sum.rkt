17f484de57894c138c16490aa5491619b224dfd6
ktunprasert <ktunprasert@outlook.com>
2025-06-04 14:47:03 +0100

    1-two-sum: seems ok

--- 1-two-sum.rkt
+++ 1-two-sum.rkt
@@ -3,22 +3,17 @@
 (define/contract
   (two-sum nums target)
   (-> (listof exact-integer?) exact-integer? (listof exact-integer?))
-  ; body
-  ;; (list 1)
-  ;; (let (x ))
   (let loop1 ([i 0])
-    ;; (when (or (>= i (length nums)) (>= j (length nums))) error "shouldn't happen")
     (when (>= i (length nums)) (error "shouldn't happen"))
     (define current (list-ref nums i))
 
-    (let loop2 ([j i])
-      (when (>= j (length nums)) (error "shouldn't happen"))
-      (when (= (- target current) (list-ref nums j)) (list i j))
-
-      (loop2 (add1 j))
+    (let loop2 ([j (add1 i)])
+      (cond
+        [(>= j (length nums)) (loop1 (add1 i))]
+        [(= (+ current (list-ref nums j)) target) (list i j)]
+        [else (loop2 (add1 j))]
+        )
       )
-
-    (loop1 (add1 i))
     )
   )
 
