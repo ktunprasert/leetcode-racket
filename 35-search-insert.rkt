80c878b185e4d4ef32019c86864c848eac1d1634
ktunprasert <ktunprasert@outlook.com>
2025-06-04 19:05:48 +0100

    35-search-insert: wash 0ms

--- 35-search-insert.rkt
+++ 35-search-insert.rkt
@@ -5,24 +5,17 @@
   (define (bin-search nums target low high)
     (define mid (quotient (+ low high) 2))
     (define mid-val (list-ref nums mid))
-    ;; (eprintf "target: ~a\tlow: ~a, high: ~a, mid: ~a, mid-val: ~a\n" target low high mid mid-val)
-    ;; (eprintf "\t\t(h-l): ~a\n" (- high low))
-    (match (- high low)
-      [1 (cond
-           [(> target mid-val) (add1 mid)]
-           [else mid])]
-      [_
-       (cond
-         [(= mid-val target) mid]
-         [(> mid-val target) (bin-search nums target low mid)]
-         [(< mid-val target) (bin-search nums target mid high)]
-         )]
+    (cond
+      [(> low high) low]
+      [(= mid-val target) mid]
+      [(> mid-val target) (bin-search nums target low (sub1 mid))]
+      [(< mid-val target) (bin-search nums target (add1 mid) high)]
       )
     )
-  (bin-search nums target 0 (length nums))
+  (bin-search nums target 0 (sub1 (length nums)))
   )
 
-;; (search-insert '(1 3 5 6) 5) ; should be 2
-;; (search-insert '(1 3 5 6) 4) ; should be 2
-;; (search-insert '(1 3 5 6) 2) ; should be 1
+(search-insert '(1 3 5 6) 5) ; should be 2
+(search-insert '(1 3 5 6) 4) ; should be 2
+(search-insert '(1 3 5 6) 2) ; should be 1
 (search-insert '(1 3 5 6) 7) ; should be 4
