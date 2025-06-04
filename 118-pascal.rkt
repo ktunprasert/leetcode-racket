9c93f3adcb7a21f9cd97bcb85ef5fd1e76ffee5f
ktunprasert <ktunprasert@outlook.com>
2025-06-04 22:13:16 +0100

    118-pascal: 0ms washed

--- 118-pascal.rkt
+++ 118-pascal.rkt
@@ -1,29 +1,21 @@
 #lang racket
 
-
-(define (window2 lst)
-  (let loop ([lst lst] [out '()])
-    (match lst
-      [(list a b) (cons (list a b) out)]
-      [_ (loop (cdr lst) (cons (list (car lst) (cadr lst)) out))])))
-
-(define (pascal n)
-  (match n [1 '(1)] [2 '(1 1)]
-    [_ (append
-        '(1)
-        (map
-         (lambda (n) (apply + n))
-         (window2 (pascal (sub1 n))))
-        '(1))]))
+(define (pascal-optimized previous-row)
+  (if (empty? previous-row) '(1)
+      (let* ([middle (map + (build-list
+                             (sub1 (length previous-row))
+                             (lambda (i)
+                               (+ (list-ref previous-row i)
+                                  (list-ref previous-row (add1 i))))))])
+        (cons 1 (append middle '(1))))))
 
 (define/contract (generate numRows)
   (-> exact-integer? (listof (listof exact-integer?)))
-  (let loop ([n 1] [lst '()])
+  (let loop ([n 1] [current '()] [all '()])
     (cond
-      [(> n numRows) (reverse lst)]
-      [else (loop (add1 n) (cons (pascal n) lst))]
-      )
-    )
-  )
+      [(> n numRows) (reverse all)]
+      [else
+       (let ([next-row (pascal-optimized current)])
+         (loop (add1 n) next-row (cons next-row all)))])))
 
 (generate 5)
