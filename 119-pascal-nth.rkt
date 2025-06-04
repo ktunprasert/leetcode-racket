3cb961e4a1af2671f6b6d3ed21bc8db68af5c2fc
ktunprasert <ktunprasert@outlook.com>
2025-06-05 00:08:25 +0100

    119-pascal-nth: butter chicken

--- 119-pascal-nth.rkt
+++ 119-pascal-nth.rkt
@@ -8,12 +8,15 @@
 ;; '(1 1 0 0) maybe start here - when n = 2 only care about (0, 1)
 ;; '(1 2 1 0) n = 3; (0, 1, 2) -> permutation = ((0,1) (1,2))
 ;; '(1 3 3 1) (0 1 2 3) -> ((0,1) (1,2) (2,3))
-(define/contract (get-row n)
-  (-> exact-integer? (listof exact-integer?))
+;; (define/contract (get-row n)
+;;   (-> exact-integer? (listof exact-integer?))
+(define (get-row n)
   (match n
     [1 '(1)]
     [2 '(1 1)]
-    [_ (let loop ([i 2] [arr (vector->list (vector-extend #(1 1) (add1 n)))])
+    ;; [_ (let loop ([i 2] [arr (append '(1 1) (make-list (add1 (quotient (add1 n) 2)) 0))])
+    [_ (let loop ([i 2] [arr (append '(1 1) ((compose (curryr make-list 0) add1 (curryr quotient 2) add1) n))])
+    ;; [_ (let loop ([i 2] [arr (append '(1 1) ((curryr compose add1 (curryr quotient 2) add1 (curryr make-list 0)) n))])
          (cond
            [(> i n) arr]
            [else (loop
