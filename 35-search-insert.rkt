c3e3c68db152428c0239ff1c608c1d75c76cafad
ktunprasert <ktunprasert@outlook.com>
2025-06-04 18:43:04 +0100

    35-search-insert: unwashed

---
+++ 35-search-insert.rkt
@@ -0,0 +1,31 @@
+#lang racket
+
+(define/contract (search-insert nums target)
+  (-> (listof exact-integer?) exact-integer? exact-integer?)
+  (define (bin-search nums target low high)
+    (define mid (quotient (+ low high) 2))
+    (define mid-val (list-ref nums mid))
+    (eprintf "target: ~a\tlow: ~a, high: ~a, mid: ~a, mid-val: ~a\n" target low high mid mid-val)
+    (eprintf "\t\t(h-l): ~a\n" (- high low))
+    (match (- high low)
+      ;; [1 (cond
+      ;;      [(> target (list-ref nums high)) (add1 high)]
+      ;;      [(< target (list-ref nums low)) (sub1 low)]
+      ;;      [else mid]
+      ;;      )]
+      [1 -1]
+      [n #:when (< n 0) -1]
+      ;; [2 mid]
+      [_ (
+          cond
+           [(= mid-val target) mid]
+           [(< mid-val target) (bin-search nums target (add1 mid) high)]
+           [(> mid-val target) (bin-search nums target low (sub1 mid))]
+           )]))
+  (bin-search nums target 0 (sub1 (length nums) ))
+  )
+
+;; (search-insert '(1 3 5 6) 5) ; should be 2
+(search-insert '(1 3 5 6) 4) ; should be 2
+;; (search-insert '(1 3 5 6) 2) ; should be 1
+;; (search-insert '(1 3 5 6) 7) ; should be 4
