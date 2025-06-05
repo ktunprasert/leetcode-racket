f8ec4217c1d903422f4c9ea1a2a25644c022eac5
ktunprasert <ktunprasert@outlook.com>
2025-06-05 14:25:04 +0100

    1920-arr-from-perm: wew 29ms

---
+++ 1920-arr-from-perm.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(require rackunit)
+
+; build-array : nums -> nums
+; nums: list-of exact-integer
+; 1 <= nums.length <= 1000
+; 0 <= nums[i] <= nums.length
+(define (build-array nums)
+  (map (curry list-ref nums) nums)
+  )
+
+
+(check-equal? (build-array '(0 2 1 5 3 4)) '(0 1 2 4 5 3))
+(check-equal? (build-array '(5 0 1 2 3 4)) '(4 5 0 1 2 3))
