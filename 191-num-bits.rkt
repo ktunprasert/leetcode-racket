d467acf8248b14df11dfc082ecb7f9c491d3668a
ktunprasert <ktunprasert@outlook.com>
2025-06-05 12:22:21 +0100

    191-num-bits: ok 0ms elegant one shot easy clap

---
+++ 191-num-bits.rkt
@@ -0,0 +1,12 @@
+#lang racket
+
+(define (hamming-weight n)
+  (let loop ([n n] [count 0])
+    (match n [0 count]
+      [_ (loop (bitwise-and n (sub1 n)) (add1 count))])
+))
+
+
+(hamming-weight 11)
+(hamming-weight 128)
+(hamming-weight 2147483645)
