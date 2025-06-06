e9e9d6e79ad002ce032d0de1a691546699a285b5
ktunprasert <ktunprasert@outlook.com>
2025-06-06 14:52:28 +0100

    2181: wip

--- 2181-merge-linked-list.rkt
+++ 2181-merge-linked-list.rkt
@@ -1,19 +1,36 @@
 #lang racket
 
-; Definition for singly-linked list:
-
-; val : integer?
-; next : (or/c list-node? #f)
-(struct list-node
-  (val next) #:mutable #:transparent)
-
-; constructor
-(define (make-list-node [val 0])
-  (list-node val #f))
-
+(require "list-node-helper.rkt")
 
 (define (merge-nodes head)
-  ;; (-> (or/c list-node? #f) (or/c list-node? #f))
+  ;; (println head)
+  (define merged-head #f)
 
-  #f
+  (let loop ([current head] [sum 0] [merged #f] )
+    (match current
+      ;; [#f merged]
+      [#f (begin (println merged) merged)]
+      [_
+       (match (list-node-val current )
+         [0 (loop (list-node-next current) 0
+                  (begin
+                    (if (eq? #f merged)
+                        ;; (make-list-node sum)
+                        (begin
+                          (set! merged-head (make-list-node sum))
+                          (make-list-node sum)
+                          )
+                        (begin
+                          (set-list-node-next! merged (make-list-node sum))
+                          (list-node-next merged)
+                          ))
+
+                    ))]
+         [n (loop (list-node-next current) (+ sum n) merged)]
+         )
+       ] ) )
+
+  merged-head
   )
+
+(merge-nodes (build-list-nodes '(0 1 1 0 1 0)))
