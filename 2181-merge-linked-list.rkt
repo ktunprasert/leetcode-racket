74cf7e0a5cb819ab8242b501caeeb33ed720d0a6
ktunprasert <ktunprasert@outlook.com>
2025-06-06 15:35:33 +0100

    2181: ok 6-19ms

--- 2181-merge-linked-list.rkt
+++ 2181-merge-linked-list.rkt
@@ -3,27 +3,20 @@
 (require "list-node-helper.rkt")
 
 (define (merge-nodes head)
-  (println "begin")
-  (println head)
-
   (let loop ([next (list-node-next head)]
-             [current head])
-    (when (not (eq? #f next))
-      (begin
-        ;; (eprintf "\t next~a current~a\n" (list-node-val next) (list-node-val current))
+             [current head]
+             [prev head])
+    (if (not (eq? #f next))
         (match (list-node-val next)
-          [0
-           (begin
-             ;; (set-list-node-next! current (list-node-next next))
-             (loop (list-node-next next) (list-node-next current)))]
+          [0 (loop (list-node-next next) (list-node-next current) current)]
           [n
            (begin
              (set-list-node-val! current (+ n (list-node-val current)))
              (set-list-node-next! current (list-node-next next))
-             ;; (loop (list-node-next next) (list-node-next current)))]))]))
-             (loop (list-node-next next) current))]))))
-
+             (loop (list-node-next next) current current))])
+        (set-list-node-next! prev #f)))
   head)
 
 ;; (merge-nodes (build-list-nodes '(0 1 1 0)))
-(merge-nodes (build-list-nodes '(0 1 1 0 1 0)))
+;; (merge-nodes (build-list-nodes '(0 1 1 0 1 0)))
+(merge-nodes (build-list-nodes '(0 1 0 1 0 1 0)))
