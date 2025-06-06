4e4191b44c4a20c287b3428d2b98d516d0374cd8
ktunprasert <ktunprasert@outlook.com>
2025-06-06 19:45:09 +0100

    Revert "1689: all that work for 80ms"
    This reverts commit 07013035cdbd68029e1ca11ddd5f0b27f64cb055.

--- 1689-partition-deci-binary.rkt
+++ 1689-partition-deci-binary.rkt
@@ -1,28 +1,11 @@
 #lang racket
 
-(define (cint c)
-  (match c
-    [#\0 0]
-    [#\1 1]
-    [#\2 2]
-    [#\3 3]
-    [#\4 4]
-    [#\5 5]
-    [#\6 6]
-    [#\7 7]
-    [#\8 8]
-    [#\9 9]))
-
 ; first glance - just find maximum char and return that
-(define (min-partitions str)
-  (let loop ([max-char 0]
-             [next (string->list str)])
-    (if (null? next)
-        max-char
-        (match (car next)
-          [#\9 9]
-          [#\0 (loop max-char (cdr next))]
-          [c (loop (max max-char (cint c)) (cdr next))]))))
+(define (min-partitions n)
+  (for/fold ([max-char 0])
+            ([c n]
+             #:when (not (char=? #\0 c)))
+    (values (max (- (char->integer c) 48) max-char))))
 
 (min-partitions "32")
 (min-partitions "10000000000000000000000000009")
