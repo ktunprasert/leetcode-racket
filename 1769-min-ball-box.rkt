f907686b4b70d684b6e7456d2f96a293c329b021
ktunprasert <ktunprasert@outlook.com>
2025-06-06 02:32:53 +0100

    1769: lmao claude thinks this is fastest its O(N^2)

--- 1769-min-ball-box.rkt
+++ 1769-min-ball-box.rkt
@@ -32,8 +32,27 @@
   (vector->list ops)
   )
 
+;; lmao nice try claude this shit is 300ms+
+(define (min-operations-fastest boxes)
+  (define len (string-length boxes))
+
+  (define ones-pos
+    (for/list ([i (in-range len)]
+               #:when (char=? (string-ref boxes i) #\1))
+      i))
+
+  (define ones-count (length ones-pos))
+  (if (zero? ones-count)
+      (make-list len 0)
+      (for/list ([target (in-range len)])
+        (for/sum ([pos ones-pos])
+          (abs (- pos target))))))
+
 (require rackunit)
 
 ;; (min-operations "110")
 (check-equal? (min-operations "110") '(1 1 3))
 (check-equal? (min-operations "001011") '(11 8 5 4 3 4))
+
+(check-equal? (min-operations-fastest "110") '(1 1 3))
+(check-equal? (min-operations-fastest "001011") '(11 8 5 4 3 4))
