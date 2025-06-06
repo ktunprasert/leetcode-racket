57d0b752a63af881b7e62c50324be22756e54a5c
ktunprasert <ktunprasert@outlook.com>
2025-06-06 02:48:05 +0100

    2469: sleeping

---
+++ 2469-convert-celsius.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+(define (convert-temperature celsius)
+  (list (+ 273.15 celsius) (+ 32. (* 1.8 celsius) ) )
+  )
+
+(require rackunit)
+
+(check-equal? (convert-temperature 0) (list 273.15 32.))
+(check-equal? (convert-temperature 100) (list 373.15 212.0))
+;; (check-equal? (convert-temperature -40) (list 233.15 -40.0))
