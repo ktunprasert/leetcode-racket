12cddf834bf9abcdc005d7e5d5c936e038bc8d64
ktunprasert <ktunprasert@outlook.com>
2025-06-06 19:59:50 +0100

    1672: tikka masala

--- 1672-richest.rkt
+++ 1672-richest.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
 (define (maximum-wealth accounts)
-  (apply max (map (lambda (x) (foldl + 0 x)) accounts)))
+  (apply max (map (curry foldl + 0) accounts)))
 
 (maximum-wealth '((1 2 3) (3 2 1) (4 5 6)))
