06c94c868b40d0cf5d3df5c87dfad906d3ab11c9
ktunprasert <ktunprasert@outlook.com>
2025-06-06 15:28:33 +0100

    2181: wip still have one off right side node

--- 2181-merge-linked-list.rkt
+++ 2181-merge-linked-list.rkt
@@ -3,34 +3,27 @@
 (require "list-node-helper.rkt")
 
 (define (merge-nodes head)
-  ;; (println head)
-  (define merged-head #f)
+  (println "begin")
+  (println head)
 
-  (let loop ([current head] [sum 0] [merged #f] )
-    (match current
-      ;; [#f merged]
-      [#f (begin (println merged) merged)]
-      [_
-       (match (list-node-val current )
-         [0 (loop (list-node-next current) 0
-                  (begin
-                    (if (eq? #f merged)
-                        ;; (make-list-node sum)
-                        (begin
-                          (set! merged-head (make-list-node sum))
-                          (make-list-node sum)
-                          )
-                        (begin
-                          (set-list-node-next! merged (make-list-node sum))
-                          (list-node-next merged)
-                          ))
+  (let loop ([next (list-node-next head)]
+             [current head])
+    (when (not (eq? #f next))
+      (begin
+        ;; (eprintf "\t next~a current~a\n" (list-node-val next) (list-node-val current))
+        (match (list-node-val next)
+          [0
+           (begin
+             ;; (set-list-node-next! current (list-node-next next))
+             (loop (list-node-next next) (list-node-next current)))]
+          [n
+           (begin
+             (set-list-node-val! current (+ n (list-node-val current)))
+             (set-list-node-next! current (list-node-next next))
+             ;; (loop (list-node-next next) (list-node-next current)))]))]))
+             (loop (list-node-next next) current))]))))
 
-                    ))]
-         [n (loop (list-node-next current) (+ sum n) merged)]
-         )
-       ] ) )
+  head)
 
-  merged-head
-  )
-
+;; (merge-nodes (build-list-nodes '(0 1 1 0)))
 (merge-nodes (build-list-nodes '(0 1 1 0 1 0)))
