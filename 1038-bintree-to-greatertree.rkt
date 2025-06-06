2aea48b810a4e51d565c25be55838cc92dad7cc2
ktunprasert <ktunprasert@outlook.com>
2025-06-06 21:56:26 +0100

    1038: washed

--- 1038-bintree-to-greatertree.rkt
+++ 1038-bintree-to-greatertree.rkt
@@ -3,35 +3,19 @@
 (require "bin-tree-helper.rkt")
 
 (define (bst-to-gst root)
-  (define shish (make-hash))
   (define sum 0)
   (let loop ([node root])
-    (if (eq? #f node)
-        #f
-        (begin
-          (let ([n (tree-node-val node)])
-            (begin
-              (hash-set! shish n 0)
-              (set! sum (+ sum n))))
+    (when (not (eq? #f node))
+      (begin
+        (loop (tree-node-right node))
 
-          (loop (tree-node-left node))
-          (loop (tree-node-right node)))))
+        (let ([n (tree-node-val node)])
+          (begin
+            (set-tree-node-val! node (+ n sum))
+            (set! sum (+ sum n))))
 
-  (let ([all (hash-keys shish)])
-    (for ([n all])
-      (hash-set! shish n (- sum (apply + (filter (curryr < n) all))))))
-
-  (let loop ([node root])
-    (if (eq? #f node)
-        #f
-        (begin
-          (let ([n (tree-node-val node)])
-            (begin
-              (set-tree-node-val! node (hash-ref shish n))))
-          (loop (tree-node-left node))
-          (loop (tree-node-right node)))))
-
+        (loop (tree-node-left node)))))
   root)
 
-(bst-to-gst (build-tree-in-order '(5 1 7)))
+;; (bst-to-gst (build-tree-in-order '(5 1 7)))
 (bst-to-gst (build-tree-in-order '(4 1 6 0 2 5 7 3 8)))
