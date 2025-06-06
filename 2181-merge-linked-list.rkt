bc4bf49841cf66da596721f7a62a2ef89f79ab89
ktunprasert <ktunprasert@outlook.com>
2025-06-06 15:51:00 +0100

    2181: ok worse performance

--- 2181-merge-linked-list.rkt
+++ 2181-merge-linked-list.rkt
@@ -3,19 +3,22 @@
 (require "list-node-helper.rkt")
 
 (define (merge-nodes head)
+  (define new-head (make-list-node 0))
   (let loop ([next (list-node-next head)]
-             [current head]
-             [prev head])
-    (if (not (eq? #f next))
-        (match (list-node-val next)
-          [0 (loop (list-node-next next) (list-node-next current) current)]
-          [n
-           (begin
-             (set-list-node-val! current (+ n (list-node-val current)))
-             (set-list-node-next! current (list-node-next next))
-             (loop (list-node-next next) current current))])
-        (set-list-node-next! prev #f)))
-  head)
+             [current new-head])
+    (when (not (eq? #f next))
+      (match (list-node-val next)
+        [0 #:when (not (eq? #f (list-node-next next)))
+         (begin
+           (define current-next (make-list-node 0))
+           (set-list-node-next! current current-next)
+           (loop (list-node-next next) current-next))]
+        [n
+         (begin
+           (set-list-node-val! current (+ n (list-node-val current)))
+           (set-list-node-next! current (list-node-next next))
+           (loop (list-node-next next) current))])))
+  new-head)
 
 (require rackunit)
 
