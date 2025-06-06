854f1ad95b7eb940e3013e3ba45fd64887d444b7
ktunprasert <ktunprasert@outlook.com>
2025-06-06 22:26:59 +0100

    2433: worse memory

--- 2433-origin-xor-prefix.rkt
+++ 2433-origin-xor-prefix.rkt
@@ -2,11 +2,13 @@
 
 (define/contract (find-array pref)
   (-> (listof exact-integer?) (listof exact-integer?))
-  (define prefix 0)
-  (for/list ([n pref])
-    (begin
-      (define new (bitwise-xor prefix n))
-      (set! prefix (bitwise-xor prefix new))
-      new)))
+  (let loop ([cache 0]
+             [rest pref]
+             [out '()])
+    (if (null? rest)
+        (reverse out)
+        (let* ([n (car rest)]
+               [new (bitwise-xor cache n)])
+          (loop (bitwise-xor cache new) (cdr rest) (cons new out))))))
 
-(find-array '(5 2 0 3 1))
+(find-array '(5 2 0 3 1)) ; 5,7,2,3,2
