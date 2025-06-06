9c7aabaff00c69b91b5834bdb6d51168894f5170
ktunprasert <ktunprasert@outlook.com>
2025-06-06 19:47:47 +0100

    1689: 0-21ms hmm

--- 1689-partition-deci-binary.rkt
+++ 1689-partition-deci-binary.rkt
@@ -2,10 +2,11 @@
 
 ; first glance - just find maximum char and return that
 (define (min-partitions n)
-  (for/fold ([max-char 0])
+  (for/fold ([max-char #\0]
+             #:result (- (char->integer max-char) 48))
             ([c n]
              #:when (not (char=? #\0 c)))
-    (values (max (- (char->integer c) 48) max-char))))
+    (values (if (char>? c max-char) c max-char))))
 
 (min-partitions "32")
 (min-partitions "10000000000000000000000000009")
