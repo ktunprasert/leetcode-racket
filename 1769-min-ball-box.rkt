80bdafd8b29840341cbc792a4bc08e4d0d098a51
ktunprasert <ktunprasert@outlook.com>
2025-06-06 02:10:12 +0100

    1769: 0-10ms washing

--- 1769-min-ball-box.rkt
+++ 1769-min-ball-box.rkt
@@ -4,10 +4,6 @@
 ;;   (-> string? (listof exact-integer?))
 ;;   )
 
-;; one pass, vector size (length boxes)
-;; when current is one broadcast to all the vector the dist where dist = (n - i)
-;; 0 skip
-;; finally convert
 (define (min-operations boxes)
   (define len (string-length boxes))
   (define ops (vector-extend #() len))
@@ -16,32 +12,15 @@
   (define (prefix-loop fn [i 0] [end len] [found 0] [idx-sum 0])
     (if (= i end) '()
         (begin
-          ;; (println i)
-          ;; (eprintf "i: ~a, found: ~a, idx-sum: ~a\n" i found idx-sum)
-
-          (vector-set! ops i (+ (vector-ref ops i) (abs (- (* i found) idx-sum)) ))
+          (vector-set! ops i (+ (vector-ref ops i) (abs (- (* i found) idx-sum))))
           (match (vector-ref box-vec i)
-            [#\0
-             (begin
-               (prefix-loop fn (fn i) end found idx-sum)
-               )]
-            [#\1
-             (begin
-               (prefix-loop fn (fn i) end (add1 found) (+ idx-sum i))
-               )]
-            ))
-        )
-    )
+            [#\0 (begin (prefix-loop fn (fn i) end found idx-sum))]
+            [#\1 (begin (prefix-loop fn (fn i) end (add1 found) (+ idx-sum i)))]
+            ))))
 
   (prefix-loop add1)
-  ;; (println ops)
   (prefix-loop sub1 (sub1 len) -1)
-  (println ops)
-  ;; (string->list)
-  ;; (string-reverse)
-
   (vector->list ops )
-  ;; ()
   )
 
 (require rackunit)
