18ab595e794d4be7623044a2fc34fd1e7a5d7300
ktunprasert <ktunprasert@outlook.com>
2025-06-07 23:42:02 +0100

    2044: with immut hash lol probably way slower

--- 2044-count-max-or-subset.rkt
+++ 2044-count-max-or-subset.rkt
@@ -1,11 +1,12 @@
 #lang racket
 
 (define (count-max-or-subsets nums)
-  (define max-or (apply bitwise-ior nums))
-  (for/fold ([count 0])
-            ([lst (in-combinations nums)]
-             #:when (= max-or (apply bitwise-ior lst)))
-    (add1 count)))
+  (for/fold ([hash (make-immutable-hash)]
+             [max-key 0]
+             #:result (hash-ref hash max-key)
+             )
+            ([lst (in-combinations nums)])
+    (let ([key (apply bitwise-ior lst)]) (values (hash-update hash key add1 0) (max max-key key)))))
 
 (count-max-or-subsets '(3 1))
 (count-max-or-subsets '(2 2 2))
