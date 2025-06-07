40895e9603a1accac096231c7710966a88ccd9a7
ktunprasert <ktunprasert@outlook.com>
2025-06-07 23:57:40 +0100

    2044: how the fuck is this the worst performance

--- 2044-count-max-or-subset.rkt
+++ 2044-count-max-or-subset.rkt
@@ -2,10 +2,16 @@
 
 (define (count-max-or-subsets nums)
   (define max-or (apply bitwise-ior nums))
-  (for/fold ([count 0])
-            ([lst (in-combinations nums)]
-             #:when (= max-or (apply bitwise-ior lst)))
-    (add1 count)))
+  (let loop ([count 0]
+             [combs (combinations nums)])
+    (if (empty? combs)
+        count
+        (let bit-loop ([inner-max-or 0]
+                       [lst (first combs)])
+          (cond
+            [(empty? lst) (loop (+ count (if (= max-or inner-max-or) 1 0)) (rest combs))]
+            [(= inner-max-or max-or) (loop (add1 count) (rest combs))]
+            [else (bit-loop (bitwise-ior inner-max-or (first lst)) (rest lst))])))))
 
 (count-max-or-subsets '(3 1))
 (count-max-or-subsets '(2 2 2))
