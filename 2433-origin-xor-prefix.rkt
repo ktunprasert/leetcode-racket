c349472c5ecf2f5b5a8eb22ed09a4dc4a5bb1b30
ktunprasert <ktunprasert@outlook.com>
2025-06-07 22:49:11 +0100

    2433: learning about begin0

--- 2433-origin-xor-prefix.rkt
+++ 2433-origin-xor-prefix.rkt
@@ -4,9 +4,7 @@
   (-> (listof exact-integer?) (listof exact-integer?))
   (define prefix 0)
   (for/list ([n pref])
-    (begin
-      (define new (bitwise-xor prefix n))
-      (set! prefix (bitwise-xor prefix new))
-      new)))
+    (begin0 (bitwise-xor prefix n)
+      (set! prefix (bitwise-xor prefix (bitwise-xor prefix n))))))
 
 (find-array '(5 2 0 3 1))
