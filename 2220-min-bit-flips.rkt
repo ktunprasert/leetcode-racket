0e46b7a745926041aa0ff0310f4d16263747bbfe
ktunprasert <ktunprasert@outlook.com>
2025-06-08 00:30:04 +0100

    2220: ok i mean why didnt i stick with it

--- 2220-min-bit-flips.rkt
+++ 2220-min-bit-flips.rkt
@@ -5,27 +5,9 @@
       acc
       (bit-count (bitwise-and n (sub1 n)) (add1 acc))))
 
-;; (define/contract (min-bit-flips start goal)
-;;   (-> exact-integer? exact-integer? exact-integer?)
-;;   (bit-count (abs (- start goal) )))
-
-;; (flip 1 0) ; 1
-;; (flip 1 1) ; 0
-;; (flip 0 0) ; 0
-;; (flip 4 1) ; 1
-;; (flip 5 1) ; 0
-;; (andor 4 1) ; 1
-
-(define flip (compose (curry bitwise-and 1) bitwise-xor))
-
-(define (min-bit-flips start goal [acc 0])
-  (case (list start goal)
-    [(0 0) acc]
-    [((1 0) (0 1)) (add1 acc)]
-    [else
-     (min-bit-flips (arithmetic-shift start -1)
-                    (arithmetic-shift goal -1)
-                    (+ acc (flip start goal)))]))
+(define (min-bit-flips start goal)
+  (bit-count (bitwise-xor start goal))
+  )
 
 (min-bit-flips 1 0)
 (min-bit-flips 10 7)
