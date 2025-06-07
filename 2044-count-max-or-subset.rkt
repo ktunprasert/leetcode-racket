eb8659649b618453cc095173265ccb86574edbd3
ktunprasert <ktunprasert@outlook.com>
2025-06-07 23:37:01 +0100

    2044: ok but 200ms

---
+++ 2044-count-max-or-subset.rkt
@@ -0,0 +1,12 @@
+#lang racket
+
+(define (count-max-or-subsets nums)
+  (define max-or (apply bitwise-ior nums))
+  (for/fold ([count 0])
+            ([lst (in-combinations nums)]
+             #:when (= max-or (apply bitwise-ior lst)))
+    (add1 count)))
+
+(count-max-or-subsets '(3 1))
+(count-max-or-subsets '(2 2 2))
+(count-max-or-subsets '(3 2 1 5))
