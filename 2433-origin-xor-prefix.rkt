442b6728cef5fe0de730b7941e0adc630aff2c3f
ktunprasert <ktunprasert@outlook.com>
2025-06-07 23:01:28 +0100

    2433: use for/fold and #:result

--- 2433-origin-xor-prefix.rkt
+++ 2433-origin-xor-prefix.rkt
@@ -1,10 +1,10 @@
 #lang racket
 
-(define/contract (find-array pref)
-  (-> (listof exact-integer?) (listof exact-integer?))
-  (define prefix 0)
-  (for/list ([n pref])
-    (begin0 (bitwise-xor prefix n)
-      (set! prefix (bitwise-xor prefix (bitwise-xor prefix n))))))
+(define (find-array pref)
+  (for/fold ([out empty]
+             [prefix 0]
+             #:result (reverse out))
+            ([n (in-list pref)])
+    (values (cons (bitwise-xor prefix n) out) (bitwise-xor prefix (bitwise-xor prefix n)))))
 
 (find-array '(5 2 0 3 1))
