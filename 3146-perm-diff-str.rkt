58a44c05381e7842db204aa2e9e0a9b8514f61c3
ktunprasert <ktunprasert@outlook.com>
2025-06-08 14:13:09 +0100

    3146: 0ms - need to get a hold of what is "fast" - apparently immut hash not great

--- 3146-perm-diff-str.rkt
+++ 3146-perm-diff-str.rkt
@@ -1,14 +1,13 @@
 #lang racket
 
 (define (find-permutation-difference s t)
-  (for/fold ([h (make-immutable-hash)]
-             #:result (apply + (hash-values h)))
-            ([c1 (in-string s)]
-             [c2 (in-string t)]
-             [i (in-naturals)])
-    (let* ([h (hash-update h c1 (curry - i) 0)]
-           [h (hash-update h c2 (curry - i) 0)])
-      h)))
+  (let ([h-s (for/hash ([c (in-string s)]
+                        [i (in-naturals)])
+               (values c i))]
+        [h-t (for/hash ([c (in-string t)]
+                        [i (in-naturals)])
+               (values c i))])
+    (for/sum ([c (in-hash-keys h-s)]) (abs (- (hash-ref h-s c) (hash-ref h-t c))))))
 
 (find-permutation-difference "abc" "bac")
 (find-permutation-difference "abcde" "edbac")
