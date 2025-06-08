3388cf61a2a19ba9bb692cbcac6f777760a5d9f5
ktunprasert <ktunprasert@outlook.com>
2025-06-08 01:49:19 +0100

    938: wash

--- 938-range-sum-bst.rkt
+++ 938-range-sum-bst.rkt
@@ -5,12 +5,11 @@
 (define (range-sum-bst root low high)
   (if (eq? root #f)
       0
-      (begin
-        (let* ([node-val (tree-node-val root)]
-               [node-acc (if (and (<= node-val high) (>= node-val low)) node-val 0)]
-               [right-acc (range-sum-bst (tree-node-right root) low high)]
-               [left-acc (range-sum-bst (tree-node-left root) low high)])
-           (+ node-acc right-acc left-acc)))))
+      (let* ([node-val (tree-node-val root)]
+             [node-acc (if (and (<= node-val high) (>= node-val low)) node-val 0)]
+             [right-acc (range-sum-bst (tree-node-right root) low high)]
+             [left-acc (range-sum-bst (tree-node-left root) low high)])
+        (+ node-acc right-acc left-acc))))
 
-(range-sum-bst (build-tree-in-order '(1 2 3)) 1 3); 6
-(range-sum-bst (build-tree-in-order '(10 5 15 3 7 18)) 7 15); 32
+(range-sum-bst (build-tree-in-order '(1 2 3)) 1 3) ; 6
+(range-sum-bst (build-tree-in-order '(10 5 15 3 7 18)) 7 15) ; 32
