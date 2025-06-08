fac0922c8973e12a97a1e2f2a6a2466d98816c52
ktunprasert <ktunprasert@outlook.com>
2025-06-08 01:47:52 +0100

    938: in order sum

---
+++ 938-range-sum-bst.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+(require "bin-tree-helper.rkt")
+
+(define (range-sum-bst root low high)
+  (if (eq? root #f)
+      0
+      (begin
+        (let* ([node-val (tree-node-val root)]
+               [node-acc (if (and (<= node-val high) (>= node-val low)) node-val 0)]
+               [right-acc (range-sum-bst (tree-node-right root) low high)]
+               [left-acc (range-sum-bst (tree-node-left root) low high)])
+           (+ node-acc right-acc left-acc)))))
+
+(range-sum-bst (build-tree-in-order '(1 2 3)) 1 3); 6
+(range-sum-bst (build-tree-in-order '(10 5 15 3 7 18)) 7 15); 32
