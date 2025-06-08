bea1a524e4c7e9220bec643d4cfa2589ed31921c
ktunprasert <ktunprasert@outlook.com>
2025-06-08 01:20:38 +0100

    3541: backwashing

--- 3541-most-freq-vowel.rkt
+++ 3541-most-freq-vowel.rkt
@@ -1,9 +1,10 @@
 #lang racket
 
 (define (max-freq-sum s)
-  (for/fold ([vowels (make-immutable-hash '([#\? . 0]))]
-             [consonants (make-immutable-hash '([#\? . 0]))]
-             #:result (+ (apply max (hash-values consonants)) (apply max (hash-values vowels))))
+  (for/fold ([vowels (make-immutable-hash)]
+             [consonants (make-immutable-hash)]
+             #:result (+ (apply max 0 (hash-values consonants))
+                         (apply max 0 (hash-values vowels))))
             ([s (in-string s)])
     (case s
       [(#\a #\e #\i #\o #\u) (values (hash-update vowels s add1 0) consonants)]
