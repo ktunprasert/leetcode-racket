e5ca77150092c0bfc51acccdc4c497839f6bf41c
ktunprasert <ktunprasert@outlook.com>
2025-06-08 14:58:56 +0100

    1365: same slow

--- 1365-num-small-current-num.rkt
+++ 1365-num-small-current-num.rkt
@@ -3,9 +3,9 @@
 ; max can only be (count distinct) num
 ; sort reverse and minus one each time is your hash-map
 (define (smaller-numbers-than-current nums)
-  (define vsorted (list->vector (sort nums <)))
+  (define vsorted (sort nums <))
   (for/list ([n nums])
-    (vector-member n vsorted)))
+    (index-of vsorted n)))
 
 ;; (smaller-numbers-than-current '(8 1 2 2 3))
-(smaller-numbers-than-current '(60 14 3 39 49 43 53 24 33 13 32 93 65 26 77 55 2 28 2 50 18 4 92 20 57 90 64 86 54 69 28 80 88 66 57 28 67 83 3 50 86))
+;; (smaller-numbers-than-current '(60 14 3 39 49 43 53 24 33 13 32 93 65 26 77 55 2 28 2 50 18 4 92 20 57 90 64 86 54 69 28 80 88 66 57 28 67 83 3 50 86))
