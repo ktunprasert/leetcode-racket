e782df7693e934e7d6751a73b5d4b09c505a1f65
ktunprasert <ktunprasert@outlook.com>
2025-06-10 00:36:59 +0100

    1281: do block is pretty cool

--- 1281-subtract-prod-sum.rkt
+++ 1281-subtract-prod-sum.rkt
@@ -1,11 +1,10 @@
 #lang racket
 
-(define (subtract-product-and-sum n [sum 0] [product 1])
-  (if (= 0 n)
-      (- product sum)
-      (subtract-product-and-sum (quotient n 10)
-                                (+ sum (remainder n 10))
-                                (* product (remainder n 10)))))
+(define (subtract-product-and-sum n)
+  (do ([num n (quotient num 10)]
+       [sum 0 (+ sum (remainder num 10))]
+       [product 1 (* product (remainder num 10))])
+      ((= num 0) (- product sum))))
 
 (subtract-product-and-sum 234)
 ;; (subtract-product-and-sum 234)
