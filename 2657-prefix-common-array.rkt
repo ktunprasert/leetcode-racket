60060b34b63292290f8775db17202577d0fccd8a
ktunprasert <ktunprasert@outlook.com>
2025-06-09 14:09:45 +0100

    2657: everything pure vector 0ms

--- 2657-prefix-common-array.rkt
+++ 2657-prefix-common-array.rkt
@@ -1,18 +1,14 @@
 #lang racket
 
 (define (find-the-prefix-common-array a b)
-  (define h (make-hash))
-  (define result (make-vector (length a)))
-  (define count 0)
-  (for ([i (in-naturals)]
-        [x a]
-        [y b])
-    (hash-set! h x (+ 1 (hash-ref h x 0)))
-    (when (= 2 (hash-ref h x)) (set! count (+ count 1)))
-    (hash-set! h y (+ 1 (hash-ref h y 0)))
-    (when (= 2 (hash-ref h y)) (set! count (+ count 1)))
-    (vector-set! result i count))
-  (vector->list result))
+  (define counts (make-vector 51 0))
+  (define total 0)
+  (for/list ([x a] [y b])
+    (vector-set! counts x (+ 1 (vector-ref counts x)))
+    (when (= 2 (vector-ref counts x)) (set! total (+ total 1)))
+    (vector-set! counts y (+ 1 (vector-ref counts y)))
+    (when (= 2 (vector-ref counts y)) (set! total (+ total 1)))
+    total))
 
 (find-the-prefix-common-array '(1 3 2 4) '(3 1 2 4)) ; 0 2 3 4
 (find-the-prefix-common-array '(2 3 1) '(3 1 2)) ; 0 1 3
