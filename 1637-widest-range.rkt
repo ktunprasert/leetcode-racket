3b839fa6a90c80671d98079084791593d1ec2384
ktunprasert <ktunprasert@outlook.com>
2025-06-09 13:20:30 +0100

    1637: hash map deez

--- 1637-widest-range.rkt
+++ 1637-widest-range.rkt
@@ -1,11 +1,12 @@
 #lang racket
 
 (define (max-width-of-vertical-area points)
-  (define sorted-points (sort (map (curry car) points) <))
+  (define sorted-hash
+    (for/hash ([i (in-naturals)]
+               [v (sort (map (curry car) points) <)])
+    (values i v)))
   (for/fold ([wide 0]) ([i (in-range (sub1 (length points)))])
-    (define width (- (list-ref sorted-points (add1 i)) (list-ref sorted-points i)))
-    (max wide width)
-
-    ))
+    (define width (- (hash-ref sorted-hash (add1 i)) (hash-ref sorted-hash i)))
+    (max wide width)))
 
 (max-width-of-vertical-area '((8 7) (9 9) (7 4) (9 7)))
