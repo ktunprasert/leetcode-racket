fc561bfba8d8a4ff8d1e86857e5e79b6059ce1fd
ktunprasert <ktunprasert@outlook.com>
2025-06-09 14:01:25 +0100

    2657: mutable hash is fastest, define is faster than let*

--- 2657-prefix-common-array.rkt
+++ 2657-prefix-common-array.rkt
@@ -1,19 +1,18 @@
 #lang racket
 
 (define (find-the-prefix-common-array a b)
-  (define updateit (curryr hash-update add1 -1))
-  (for/fold ([h (make-immutable-hash)]
+  (for/fold ([h (make-hash)]
              [out empty]
              [count 0]
              #:result (reverse out))
-            ([a a]
-             [b b])
-    (let* ([h (updateit h a)]
-           [left (if (= 1 (hash-ref h a)) 1 0)]
-           [h (updateit h b)]
-           [right (if (= 1 (hash-ref h b)) 1 0)]
-           [new-count (+ count left right)])
-      (values h (cons new-count out) new-count))))
+            ([x a]
+             [y b])
+    (hash-update! h x add1 0)
+    (define left (if (= 2 (hash-ref h x)) 1 0))
+    (hash-update! h y add1 0)
+    (define right (if (= 2 (hash-ref h y)) 1 0))
+    (define new-count (+ count left right))
+    (values h (cons new-count out) new-count)))
 
 (find-the-prefix-common-array '(1 3 2 4) '(3 1 2 4)) ; 0 2 3 4
 (find-the-prefix-common-array '(2 3 1) '(3 1 2)) ; 0 1 3
