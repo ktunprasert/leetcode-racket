957c7334543f47c4c973cc2aa45d3f2f6d99e9b9
ktunprasert <ktunprasert@outlook.com>
2025-06-09 13:56:47 +0100

    2657: -10ms optimiser juice

--- 2657-prefix-common-array.rkt
+++ 2657-prefix-common-array.rkt
@@ -1,16 +1,19 @@
 #lang racket
 
-(define (find-the-prefix-common-array A B)
+(define (find-the-prefix-common-array a b)
   (define updateit (curryr hash-update add1 -1))
   (for/fold ([h (make-immutable-hash)]
              [out empty]
+             [count 0]
              #:result (reverse out))
-            ([a A]
-             [b B])
-    (let* ([h h]
-           [h (updateit h a)]
-           [h (updateit h b)])
-      (values h (cons (for/sum ([v (hash-values h)] #:when (positive? v)) v) out)))))
+            ([a a]
+             [b b])
+    (let* ([h (updateit h a)]
+           [left (if (= 1 (hash-ref h a)) 1 0)]
+           [h (updateit h b)]
+           [right (if (= 1 (hash-ref h b)) 1 0)]
+           [new-count (+ count left right)])
+      (values h (cons new-count out) new-count))))
 
 (find-the-prefix-common-array '(1 3 2 4) '(3 1 2 4)) ; 0 2 3 4
 (find-the-prefix-common-array '(2 3 1) '(3 1 2)) ; 0 1 3
