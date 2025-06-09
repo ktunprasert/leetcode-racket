0ac7aa7f1277be3c6b273d60da13af0ae82ce96b
ktunprasert <ktunprasert@outlook.com>
2025-06-09 13:21:26 +0100

    1637: chore

--- 1637-widest-range.rkt
+++ 1637-widest-range.rkt
@@ -5,8 +5,8 @@
     (for/hash ([i (in-naturals)]
                [v (sort (map (curry car) points) <)])
     (values i v)))
-  (for/fold ([wide 0]) ([i (in-range (sub1 (length points)))])
+  (for/fold ([widest 0]) ([i (in-range (sub1 (length points)))])
     (define width (- (hash-ref sorted-hash (add1 i)) (hash-ref sorted-hash i)))
-    (max wide width)))
+    (max widest width)))
 
 (max-width-of-vertical-area '((8 7) (9 9) (7 4) (9 7)))
