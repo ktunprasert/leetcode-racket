c7e12fa39489ae0a9002ae6122a5670c6a97f703
ktunprasert <ktunprasert@outlook.com>
2025-06-10 00:16:34 +0100

    1221: ok

---
+++ 1221-balanced-substring.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+(define/contract (balanced-string-split s)
+  (-> string? exact-integer?)
+
+  (define total 0)
+  (for/fold ([balance 0]) ([c s])
+    (when (= 0 balance)
+      (set! total (add1 total)))
+    (case c
+      [(#\R) (add1 balance)]
+      [(#\L) (sub1 balance)]))
+
+  total)
+
+(balanced-string-split "RLRRLLRLRL")
+(balanced-string-split "RLRRRLLRLL")
+(balanced-string-split "LLLLRRRR")
+(balanced-string-split "LRLR")
