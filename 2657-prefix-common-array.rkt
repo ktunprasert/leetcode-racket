c412495d6b221e46baac620080804562aafb513a
ktunprasert <ktunprasert@outlook.com>
2025-06-09 14:04:09 +0100

    2657: move hash definition outside fold

--- 2657-prefix-common-array.rkt
+++ 2657-prefix-common-array.rkt
@@ -1,8 +1,8 @@
 #lang racket
 
 (define (find-the-prefix-common-array a b)
-  (for/fold ([h (make-hash)]
-             [out empty]
+  (define h (make-hash))
+  (for/fold ([out empty]
              [count 0]
              #:result (reverse out))
             ([x a]
@@ -12,7 +12,7 @@
     (hash-update! h y add1 0)
     (define right (if (= 2 (hash-ref h y)) 1 0))
     (define new-count (+ count left right))
-    (values h (cons new-count out) new-count)))
+    (values (cons new-count out) new-count)))
 
 (find-the-prefix-common-array '(1 3 2 4) '(3 1 2 4)) ; 0 2 3 4
 (find-the-prefix-common-array '(2 3 1) '(3 1 2)) ; 0 1 3
