3925d19d5beb11e5cde82f251d78b85b6b563843
ktunprasert <ktunprasert@outlook.com>
2025-06-09 14:08:44 +0100

    2657: vector constant time index is 9-15ms

--- 2657-prefix-common-array.rkt
+++ 2657-prefix-common-array.rkt
@@ -2,15 +2,17 @@
 
 (define (find-the-prefix-common-array a b)
   (define h (make-hash))
+  (define result (make-vector (length a)))
   (define count 0)
-  (for/list ([x a]
-             [y b])
-    (hash-update! h x add1 0)
-    (define left (if (= 2 (hash-ref h x)) 1 0))
-    (hash-update! h y add1 0)
-    (define right (if (= 2 (hash-ref h y)) 1 0))
-    (set! count (+ count left right))
-    count))
+  (for ([i (in-naturals)]
+        [x a]
+        [y b])
+    (hash-set! h x (+ 1 (hash-ref h x 0)))
+    (when (= 2 (hash-ref h x)) (set! count (+ count 1)))
+    (hash-set! h y (+ 1 (hash-ref h y 0)))
+    (when (= 2 (hash-ref h y)) (set! count (+ count 1)))
+    (vector-set! result i count))
+  (vector->list result))
 
 (find-the-prefix-common-array '(1 3 2 4) '(3 1 2 4)) ; 0 2 3 4
 (find-the-prefix-common-array '(2 3 1) '(3 1 2)) ; 0 1 3
