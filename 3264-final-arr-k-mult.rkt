4503b5f772113b1812dccbdda00c897de5ca1b87
ktunprasert <ktunprasert@outlook.com>
2025-06-09 14:38:20 +0100

    3264: heap on deez

--- 3264-final-arr-k-mult.rkt
+++ 3264-final-arr-k-mult.rkt
@@ -1,14 +1,26 @@
 #lang racket
+(require data/heap)
 
 (define (get-final-state nums k multiplier)
-  (define mul (curry * multiplier))
+  (define heap
+    (make-heap (lambda (a b) (or (< (cdr a) (cdr b)) (and (= (cdr a) (cdr b)) (< (car a) (car b)))))))
 
+  ; Initialize heap with (index . value) pairs
+  (for ([val nums]
+        [i (in-naturals)])
+    (heap-add! heap (cons i val)))
+
+  ; Perform k multiplications
   (for ([_ (in-range k)])
-    (define n-min (apply min nums))
-    (for/first ([n nums]
-                [i (in-naturals)]
-                #:when (<= n n-min))
-      (set! nums (list-update nums i mul))))
-  nums)
+    (define min-pair (heap-min heap))
+    (heap-remove-min! heap)
+    (heap-add! heap (cons (car min-pair) (* (cdr min-pair) multiplier))))
+
+  ; Reconstruct the array
+  (define result (make-vector (length nums)))
+  (for ([pair (in-heap heap)])
+    (vector-set! result (car pair) (cdr pair)))
+
+  (vector->list result))
 
 (get-final-state '(2 1 3 5 6) 5 2)
