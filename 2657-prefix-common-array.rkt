d12b9edc97b779498c956b4861055072cafdc9a6
ktunprasert <ktunprasert@outlook.com>
2025-06-09 14:07:09 +0100

    2657: fully mutable, perf still 10-20ms

--- 2657-prefix-common-array.rkt
+++ 2657-prefix-common-array.rkt
@@ -2,17 +2,15 @@
 
 (define (find-the-prefix-common-array a b)
   (define h (make-hash))
-  (for/fold ([out empty]
-             [count 0]
-             #:result (reverse out))
-            ([x a]
+  (define count 0)
+  (for/list ([x a]
              [y b])
     (hash-update! h x add1 0)
     (define left (if (= 2 (hash-ref h x)) 1 0))
     (hash-update! h y add1 0)
     (define right (if (= 2 (hash-ref h y)) 1 0))
-    (define new-count (+ count left right))
-    (values (cons new-count out) new-count)))
+    (set! count (+ count left right))
+    count))
 
 (find-the-prefix-common-array '(1 3 2 4) '(3 1 2 4)) ; 0 2 3 4
 (find-the-prefix-common-array '(2 3 1) '(3 1 2)) ; 0 1 3
