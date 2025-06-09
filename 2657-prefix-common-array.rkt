f2a6318a0513fc3e470ed409ec9389058f8873e2
ktunprasert <ktunprasert@outlook.com>
2025-06-09 13:49:09 +0100

    2657: negligible improve

--- 2657-prefix-common-array.rkt
+++ 2657-prefix-common-array.rkt
@@ -10,6 +10,7 @@
     (let* ([h h]
            [h (updateit h a)]
            [h (updateit h b)])
-      (values h (cons (apply + (hash-values h)) out)))))
+      (values h (cons (for/sum ([v (hash-values h)] #:when (positive? v)) v) out)))))
 
 (find-the-prefix-common-array '(1 3 2 4) '(3 1 2 4)) ; 0 2 3 4
+(find-the-prefix-common-array '(2 3 1) '(3 1 2)) ; 0 1 3
