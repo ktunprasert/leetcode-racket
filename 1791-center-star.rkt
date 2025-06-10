fe38abd92aeeefd8f6c2409c6e782e6d384627cd
ktunprasert <ktunprasert@outlook.com>
2025-06-10 12:56:37 +0100

    1791: ok i give up

--- 1791-center-star.rkt
+++ 1791-center-star.rkt
@@ -2,7 +2,10 @@
 
 (define/contract (find-center edges)
   (-> (listof (listof exact-integer?)) exact-integer?)
-  (match-let ([(list a b) (car edges)])
-    (if (member a (cadr edges)) a b)))
+  (let ([first-edge (car edges)]
+        [second-edge (cadr edges)])
+    (if (member (car first-edge) second-edge)
+        (car first-edge)
+        (cadr first-edge))))
 
 (find-center '((1 2) (2 3) (4 2)))
