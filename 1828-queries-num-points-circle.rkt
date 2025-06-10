c2d766c42b169d0324b6744c477cf5dadbf35b6f
ktunprasert <ktunprasert@outlook.com>
2025-06-10 13:13:56 +0100

    1828: 800ms lmfao

---
+++ 1828-queries-num-points-circle.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+(define (within-circle a b x y r)
+  (<= (expt (+ (expt (- a x) 2) (expt (- b y) 2)) 1/2) r))
+
+(define/contract (count-points points queries)
+  (-> (listof (listof exact-integer?)) (listof (listof exact-integer?)) (listof exact-integer?))
+
+  (for/list ([q queries])
+    (for/sum ([p points] #:when (within-circle (car p) (cadr p) (car q) (cadr q) (caddr q))) 1)))
+
+(count-points '((1 3) (3 3) (5 3) (2 2)) '((2 3 1) (4 3 1) (1 1 2)))
+(count-points '((1 1) (2 2) (3 3) (4 4) (5 5)) '((1 2 2) (2 2 2) (4 3 2) (4 3 3)))
