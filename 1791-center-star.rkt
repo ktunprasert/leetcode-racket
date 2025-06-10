52e737e964568974ae5ec96d9d2d7c8e4404ae50
ktunprasert <ktunprasert@outlook.com>
2025-06-10 12:53:05 +0100

    1791: same same but diff

--- 1791-center-star.rkt
+++ 1791-center-star.rkt
@@ -2,8 +2,10 @@
 
 (define/contract (find-center edges)
   (-> (listof (listof exact-integer?)) exact-integer?)
-  (match-let ([(list a b) (car edges)]
-              [(list c d) (cadr edges)])
+  (let ([a (caar edges)]
+        [b (cadar edges)]
+        [c (caadr edges)]
+        [d (cadadr edges)])
     (if (or (= a c) (= a d)) a b)))
 
 (find-center '((1 2) (2 3) (4 2)))
