b7c419b6ec3b169ab12a7d4c8f908bee3d5897d1
ktunprasert <ktunprasert@outlook.com>
2025-06-10 20:26:01 +0100

    1389: wew

--- 1389-create-target-array.rkt
+++ 1389-create-target-array.rkt
@@ -1,9 +1,10 @@
 #lang racket
 
 (define (create-target-array nums index)
-  (let ([lst (for/list ([n nums]
-                        [i index])
-               (cons i n))])
-    (map cdr (sort (reverse lst) #:key car <))))
+  (for/fold ([out empty])
+            ([n nums]
+             [i index])
+    (match-define-values (a b) (split-at out i))
+    (append a (list n) b)))
 
 (create-target-array '(0 1 2 3 4) '(0 1 2 2 1))
