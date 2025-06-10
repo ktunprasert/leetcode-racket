82dfee1f4871a19efb80d905b7a78036ddbdc140
ktunprasert <ktunprasert@outlook.com>
2025-06-10 22:02:49 +0100

    1302: that's the problem with global variables...

--- 1302-deep-leaves-sum.rkt
+++ 1302-deep-leaves-sum.rkt
@@ -5,14 +5,17 @@
 (define deepest 0)
 (define total 0)
 (define (deepest-leaves-sum root [depth 0])
+  (when (= depth 0)
+    (set! deepest 0)
+    (set! total 0))
   (cond
-    [(and (false? (tree-node-left root)) (false? (tree-node-right root)))
-     (set! deepest (max deepest depth))
-     (when (= depth deepest)
-       (set! total (+ total (tree-node-val root))))]
-    [(false? (tree-node-left root)) (deepest-leaves-sum (tree-node-right root) (add1 depth))]
-    [(false? (tree-node-right root)) (deepest-leaves-sum (tree-node-left root) (add1 depth))]
+    [(false? root) 0]
     [else
+     (when (= depth deepest)
+       (set! total (+ total (tree-node-val root))))
+     (when (> depth deepest)
+       (set! deepest depth)
+       (set! total (tree-node-val root)))
      (deepest-leaves-sum (tree-node-left root) (add1 depth))
      (deepest-leaves-sum (tree-node-right root) (add1 depth))])
   total)
@@ -20,3 +23,4 @@
 (define tn make-tree-node)
 
 (deepest-leaves-sum (tn 1 (tn 2 (tn 4 (tn 7)) (tn 5)) (tn 3 #f (tn 6 #f (tn 8)))))
+(deepest-leaves-sum (tn 6 (tn 7 (tn 2 (tn 9)) (tn 7 (tn 1) (tn 4))) (tn 8 (tn 1) (tn 3 #f (tn 5)))))
