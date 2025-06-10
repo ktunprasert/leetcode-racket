4fb590cdcdaf10770e47c67c6384c12ca894cdda
ktunprasert <ktunprasert@outlook.com>
2025-06-10 22:06:24 +0100

    1302: funktional

--- 1302-deep-leaves-sum.rkt
+++ 1302-deep-leaves-sum.rkt
@@ -2,24 +2,21 @@
 
 (require "bin-tree-helper.rkt")
 
-(define deepest (make-parameter 0))
-(define total (make-parameter 0))
 (define (deepest-leaves-sum root)
-  (parameterize ([deepest 0]
-                 [total 0])
-    (define (helper node d)
-      (cond
-        [(false? node) 0]
-        [else
-         (when (= d (deepest))
-           (total (+ (total) (tree-node-val node))))
-         (when (> d (deepest))
-           (deepest d)
-           (total (tree-node-val node)))
-         (helper (tree-node-left node) (add1 d))
-         (helper (tree-node-right node) (add1 d))]))
-    (helper root 0)
-    (total)))
+  (define (helper node depth)
+    (cond
+      [(false? node) (values -1 0)] ; depth -1 means no node, sum 0
+      [(and (false? (tree-node-left node)) (false? (tree-node-right node)))
+       (values depth (tree-node-val node))] ; leaf node
+      [else
+       (define-values (left-depth left-sum) (helper (tree-node-left node) (add1 depth)))
+       (define-values (right-depth right-sum) (helper (tree-node-right node) (add1 depth)))
+       (cond
+         [(> left-depth right-depth) (values left-depth left-sum)]
+         [(> right-depth left-depth) (values right-depth right-sum)]
+         [else (values left-depth (+ left-sum right-sum))])]))
+  (define-values (_ total) (helper root 0))
+  total)
 
 (define tn make-tree-node)
 
