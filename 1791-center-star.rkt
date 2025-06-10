e7199a46c6e4b2dfcb97463b5669a412145be68a
ktunprasert <ktunprasert@outlook.com>
2025-06-10 12:26:46 +0100

    1791: shaved 20ms

--- 1791-center-star.rkt
+++ 1791-center-star.rkt
@@ -2,16 +2,17 @@
 
 (define/contract (find-center edges)
   (-> (listof (listof exact-integer?)) exact-integer?)
-  (define edgy (make-hash))
-  (define maximum 0)
-  (define max-edge 0)
-  (for ([e edges])
-    (for ([n e])
-      (hash-update! edgy n add1 0)
-      (when (<= maximum (hash-ref edgy n))
-        (set! maximum (max maximum (hash-ref edgy n)))
-        (set! max-edge n))))
 
-  max-edge)
+  (define edgy (make-hash))
+  (define look-for (length edges))
+  (define found 0)
+
+  (for* ([e edges]
+         [n e]
+         #:do [(hash-update! edgy n add1 0) (set! found n)]
+         #:break (= (hash-ref edgy n) look-for))
+    (void))
+
+  found)
 
 (find-center '((1 2) (2 3) (4 2)))
