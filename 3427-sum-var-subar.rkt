2e57988f9a8ac4fdbfbba66d5d672881bed79785
ktunprasert <ktunprasert@outlook.com>
2025-06-17 00:30:36 +0100

    Revert "3427: slower.. 80ms"
    This reverts commit a9bac6f2a22a254940a6609da55d83954e590be5.

--- 3427-sum-var-subar.rkt
+++ 3427-sum-var-subar.rkt
@@ -1,11 +1,10 @@
 #lang racket
 
 (define (subarray-sum nums)
-  (define vnum (list->vector nums))
   (for/sum ([(n i) (in-indexed nums)])
            (define ni (- i n))
            (define maxi (max 0 ni))
-           (apply + (map (curry vector-ref vnum) (inclusive-range maxi i)))))
+           (apply + (take (drop nums maxi) (add1 (- i maxi))))))
 
 (subarray-sum '(2 3 1))
 (subarray-sum '(2 5 1 5)) ;; 28
