e8ca212b4d1ea0f9213565d0770e781e6e111111
ktunprasert <ktunprasert@outlook.com>
2025-06-17 00:26:53 +0100

    3427: 50ms

--- 3427-sum-var-subar.rkt
+++ 3427-sum-var-subar.rkt
@@ -4,8 +4,7 @@
   (for/fold ([sum 0]) ([(n i) (in-indexed nums)])
     (define ni (- i n))
     (define maxi (max 0 ni))
-    (values (apply + sum (map (curry list-ref nums) (inclusive-range maxi i))))))
+    (values (apply + sum (take (drop nums maxi) (add1 (- i maxi)))))))
 
 (subarray-sum '(2 3 1))
-;; [2,5,1,5]
 (subarray-sum '(2 5 1 5)) ;; 28
