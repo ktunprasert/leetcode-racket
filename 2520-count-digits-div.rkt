15845e04da8e3f87d7136142e91fa721f79ce45b
ktunprasert <ktunprasert@outlook.com>
2025-06-16 15:12:49 +0100

    2520: wash

--- 2520-count-digits-div.rkt
+++ 2520-count-digits-div.rkt
@@ -3,6 +3,7 @@
 (define (count-digits num [n num] [count 0])
   (cond
     [(= n 0) count]
-    [else (count-digits num (quotient n 10) (+ count (if (= 0 (modulo num (remainder n 10))) 1 0)))]))
+    [(= 0 (modulo num (remainder n 10))) (count-digits num (quotient n 10) (add1 count))]
+    [else (count-digits num (quotient n 10) count)]))
 
 (count-digits 121)
