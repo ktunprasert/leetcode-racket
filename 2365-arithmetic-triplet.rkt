16585635a58f19649b5e8cc5936bcd4eed74d857
ktunprasert <ktunprasert@outlook.com>
2025-06-17 00:46:49 +0100

    2365: 7ms weoiwueoiw

--- 2365-arithmetic-triplet.rkt
+++ 2365-arithmetic-triplet.rkt
@@ -2,7 +2,7 @@
 
 (define (arithmetic-triplets nums diff)
   (for/sum ([(n1 i1) (in-indexed nums)])
-           (for/sum ([n2 (drop nums (add1 i1))])
-                    (for/sum ([n3 (drop nums (+ 2 i1))] #:when (= diff (- n2 n1) (- n3 n2))) 1))))
+           (for/sum ([n2 (drop nums (add1 i1))] #:when (= diff (- n2 n1) ) )
+                    (for/sum ([n3 (drop nums (+ 2 i1))] #:when (= diff (- n3 n2))) 1))))
 
 (arithmetic-triplets '(0 1 4 6 7 10) 3)
