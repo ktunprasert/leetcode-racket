0a489b7d8795bad94af42546555761b9e052927c
ktunprasert <ktunprasert@outlook.com>
2025-06-16 18:09:06 +0100

    chore: fmt

--- 1021-remove-outer-paren.rkt
+++ 1021-remove-outer-paren.rkt
@@ -3,20 +3,20 @@
 (define (remove-outer-parentheses s)
   (define (helper chars depth current result)
     (cond
-      [(empty? chars)
-       (string-join (reverse result) "")]
+      [(empty? chars) (string-join (reverse result) "")]
       [(char=? (first chars) #\()
        (let ([new-depth (add1 depth)])
          (if (> new-depth 1)
-             (helper (rest chars) new-depth
-                     (string-append current (string (first chars))) result)
+             (helper (rest chars) new-depth (string-append current (string (first chars))) result)
              (helper (rest chars) new-depth current result)))]
       [(char=? (first chars) #\))
        (let ([new-depth (sub1 depth)])
          (if (= new-depth 0)
              (helper (rest chars) new-depth "" (cons current result))
-             (helper (rest chars) new-depth
-                     (string-append current (string (first chars))) result)))]))
+             (helper (rest chars)
+                     new-depth
+                     (string-append current (string (first chars)))
+                     result)))]))
 
   (helper (string->list s) 0 "" empty))
 
