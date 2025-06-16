f0363164ee6edda40e42a80883cc0cfb7c1d1ace
ktunprasert <ktunprasert@outlook.com>
2025-06-16 14:17:00 +0100

    2016: no need to guard

--- 2016-max-diff-inc-ele.rkt
+++ 2016-max-diff-inc-ele.rkt
@@ -1,17 +1,15 @@
 #lang racket
 
 (define (maximum-difference nums)
-  (if (< (length nums) 2)
-      -1
-      (for/fold ([min-so-far (first nums)]
-                 [max-diff -1]
-                 #:result max-diff)
-                ([num (in-list (rest nums))])
-        (define new-diff
-          (if (> num min-so-far)
-              (- num min-so-far)
-              -1))
-        (values (min min-so-far num) (max max-diff new-diff)))))
+  (for/fold ([min-so-far (first nums)]
+             [max-diff -1]
+             #:result max-diff)
+            ([num (in-list (rest nums))])
+    (define new-diff
+      (if (> num min-so-far)
+          (- num min-so-far)
+          -1))
+    (values (min min-so-far num) (max max-diff new-diff))))
 
 (maximum-difference '(7 1 5 4))
 (maximum-difference '(9 4 3 2))
