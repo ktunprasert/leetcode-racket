51647364608119f25d8ac1abc200a02b603e9388
ktunprasert <ktunprasert@outlook.com>
2025-06-16 14:20:36 +0100

    Revert "2016: with threading"
    This reverts commit e29683e21024294af4cdec18e9097bf7c3641c5f.

--- 2016-max-diff-inc-ele.rkt
+++ 2016-max-diff-inc-ele.rkt
@@ -1,17 +1,15 @@
 #lang racket
 
-(require threading)
-
 (define (maximum-difference nums)
   (for/fold ([min-so-far (first nums)]
              [max-diff -1]
              #:result max-diff)
             ([num (in-list (rest nums))])
-    (~> (if (> num min-so-far)
-            (- num min-so-far)
-            -1)
-        (max max-diff)
-        (values (min min-so-far num) _))))
+    (define new-diff
+      (if (> num min-so-far)
+          (- num min-so-far)
+          -1))
+    (values (min min-so-far num) (max max-diff new-diff))))
 
 (maximum-difference '(7 1 5 4))
 (maximum-difference '(9 4 3 2))
