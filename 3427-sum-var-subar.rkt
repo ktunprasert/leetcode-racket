85156719883afbdf6f5c7aca38c5455fa7bda253
ktunprasert <ktunprasert@outlook.com>
2025-06-17 00:28:15 +0100

    3427: still 50

--- 3427-sum-var-subar.rkt
+++ 3427-sum-var-subar.rkt
@@ -1,10 +1,10 @@
 #lang racket
 
 (define (subarray-sum nums)
-  (for/fold ([sum 0]) ([(n i) (in-indexed nums)])
-    (define ni (- i n))
-    (define maxi (max 0 ni))
-    (values (apply + sum (take (drop nums maxi) (add1 (- i maxi)))))))
+  (for/sum ([(n i) (in-indexed nums)])
+           (define ni (- i n))
+           (define maxi (max 0 ni))
+           (apply + (take (drop nums maxi) (add1 (- i maxi))))))
 
 (subarray-sum '(2 3 1))
 (subarray-sum '(2 5 1 5)) ;; 28
