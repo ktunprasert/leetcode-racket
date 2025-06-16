f0c6f8a1a4c85d9543b085f132a84588031de8fa
ktunprasert <ktunprasert@outlook.com>
2025-06-16 18:43:00 +0100

    2160: parameter name

--- 2160-min-sum-split.rkt
+++ 2160-min-sum-split.rkt
@@ -6,7 +6,7 @@
       [(= 0 n) nums]
       [else (get-nums (cons (remainder n 10) nums) (quotient n 10))]))
   (let*-values ([(nums) (sort (get-nums empty num) <)]
-                [(pres nums) (split-at nums 2)])
-    (foldl + 0 (append nums (map (curry * 10) pres)))))
+                [(tens singles) (split-at nums 2)])
+    (foldl + 0 (append singles (map (curry * 10) tens)))))
 
 (minimum-sum 1234)
