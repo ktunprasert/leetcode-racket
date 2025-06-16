aed987316083a81d3f154865e36726065c7f2830
ktunprasert <ktunprasert@outlook.com>
2025-06-16 23:46:39 +0100

    Revert "2391: claude copied me but got 20ms"
    This reverts commit 43f28f516c55aec4d882d876aa611f72f2cd1dfc.

--- 2391-min-collect-garbage.rkt
+++ 2391-min-collect-garbage.rkt
@@ -1,29 +1,22 @@
 #lang racket
 
+(define (take-i lst n)
+  (take lst (- (length lst) n)))
+
 (define (garbage-collection garbage travel)
-  ;; Single pass to find last indices and total garbage length
-  (define-values (Gi Pi Mi total-garbage)
-    (for/fold ([Gi #f] [Pi #f] [Mi #f] [total-len 0])
-              ([house garbage] [i (in-naturals)])
-      (values (if (string-contains? house "G") i Gi)
-              (if (string-contains? house "P") i Pi)
-              (if (string-contains? house "M") i Mi)
-              (+ total-len (string-length house)))))
+  (define gbg (reverse garbage))
+  (let ([Gi (index-where gbg (curryr string-contains? "G"))]
+        [Pi (index-where gbg (curryr string-contains? "P"))]
+        [Mi (index-where gbg (curryr string-contains? "M"))])
+    (define total 0)
+    (when Gi
+      (set! total (apply + total (take-i travel Gi))))
+    (when Pi
+      (set! total (apply + total (take-i travel Pi))))
+    (when Mi
+      (set! total (apply + total (take-i travel Mi))))
 
-  ;; Calculate travel times for each garbage type
-  ;; Build prefix sums for travel times
-  (define prefix-sums
-    (reverse (foldl (lambda (t acc)
-                      (cons (+ t (car acc)) acc))
-                    '(0)
-                    travel)))
-
-  (define travel-time
-    (+ (if Gi (list-ref prefix-sums Gi) 0)
-       (if Pi (list-ref prefix-sums Pi) 0)
-       (if Mi (list-ref prefix-sums Mi) 0)))
-
-  (+ travel-time total-garbage))
+    (apply + total (map string-length garbage))))
 
 ;; (garbage-collection (list "G" "P" "GP" "MGP") (list 2 4 3))
 (garbage-collection (list "G" "P" "GP" "GG") (list 2 4 3))
