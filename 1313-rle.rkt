30887083c9d952f7f6eaff3efe579d9bddea97f4
ktunprasert <ktunprasert@outlook.com>
2025-06-16 14:48:43 +0100

    1313: single flatten

--- 1313-rle.rkt
+++ 1313-rle.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 (define (decompress-rl-elist nums)
-  (for/fold ([out empty]) ([code (in-slice 2 nums)])
-    (append out (make-list (first code) (second code)))))
+  (flatten (for/list ([code (in-slice 2 nums)])
+             (make-list (first code) (second code)))))
 
 (decompress-rl-elist (list 1 2 3 4))
