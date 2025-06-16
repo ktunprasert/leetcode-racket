36255e937cad046d20abd3e59e0288061d195117
ktunprasert <ktunprasert@outlook.com>
2025-06-16 22:16:10 +0100

    1415: once

--- 1415-kth-lexo-happy.rkt
+++ 1415-kth-lexo-happy.rkt
@@ -4,24 +4,22 @@
 
 (define (generate-happy-strings n)
   (define result '())
-
-  (define (backtrack current-string)
+  (define (backtrack current-chars)
     (cond
-      [(= (string-length current-string) n) (set! result (cons current-string result))]
+      [(= (length current-chars) n) (set! result (cons (reverse current-chars) result))]
       [else
        (for ([c chars])
-         (when (or (= (string-length current-string) 0)
-                   (not (char=? c (string-ref current-string (sub1 (string-length current-string))))))
-           (backtrack (string-append current-string (string c)))))]))
+         (when (or (null? current-chars) (not (char=? c (first current-chars))))
+           (backtrack (cons c current-chars))))]))
 
-  (backtrack "")
+  (backtrack '())
   (reverse result))
 
 (define (get-happy-string n k)
   (define happy-strings (generate-happy-strings n))
   (if (<= k (length happy-strings))
-      (list-ref happy-strings (sub1 k)) ; Convert to 0-indexed
-      "")) ; Return empty string if k is out of bounds
+      (list->string (list-ref happy-strings (sub1 k)))
+      ""))
 
 (get-happy-string 1 1)
 (get-happy-string 3 9)
