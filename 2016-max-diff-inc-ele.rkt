e29683e21024294af4cdec18e9097bf7c3641c5f
ktunprasert <ktunprasert@outlook.com>
2025-06-16 14:20:33 +0100

    2016: with threading

--- 2016-max-diff-inc-ele.rkt
+++ 2016-max-diff-inc-ele.rkt
@@ -1,15 +1,17 @@
 #lang racket
 
+(require threading)
+
 (define (maximum-difference nums)
   (for/fold ([min-so-far (first nums)]
              [max-diff -1]
              #:result max-diff)
             ([num (in-list (rest nums))])
-    (define new-diff
-      (if (> num min-so-far)
-          (- num min-so-far)
-          -1))
-    (values (min min-so-far num) (max max-diff new-diff))))
+    (~> (if (> num min-so-far)
+            (- num min-so-far)
+            -1)
+        (max max-diff)
+        (values (min min-so-far num) _))))
 
 (maximum-difference '(7 1 5 4))
 (maximum-difference '(9 4 3 2))
