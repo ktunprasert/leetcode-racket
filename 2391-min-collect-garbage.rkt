1d355e6349d65ff226f24b1b6f42aeba8cba7bd4
ktunprasert <ktunprasert@outlook.com>
2025-06-16 23:02:22 +0100

    chore: comments

--- 2391-min-collect-garbage.rkt
+++ 2391-min-collect-garbage.rkt
@@ -1,13 +1,11 @@
 #lang racket
 
 (define (garbage-collection garbage travel)
-  ;; (define gbg (map string->list garbage))
   (let ([giga (let loop ([i (sub1 (length garbage))]
                          [s (reverse garbage)]
                          [Mi 0]
                          [Gi 0]
                          [Pi 0])
-                ;; (displayln (format "i: ~a, s: ~a, Mi: ~a, Gi: ~a, Pi: ~a" i s Mi Gi Pi))
                 (cond
                   [(empty? s) (list Mi Gi Pi)]
                   [(not (ormap (curry = 0) (list Mi Gi Pi))) (list Mi Gi Pi)]
@@ -24,8 +22,7 @@
                              (max Pi i)
                              Pi))]))])
     (+ (foldl (lambda (ti acc) (+ acc (apply + (take travel ti)))) 0 giga)
-       (apply + (map string-length garbage)))
-    ))
+       (apply + (map string-length garbage)))))
 
 ;; (garbage-collection (list "G" "P" "GP" "MGP") (list 2 4 3))
 (garbage-collection (list "G" "P" "GP" "GG") (list 2 4 3))
