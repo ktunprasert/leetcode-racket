a7d6786f0007db4aa77fa3ab26a0042459512fad
ktunprasert <ktunprasert@outlook.com>
2025-06-16 19:43:14 +0100

    Revert "2652: same"
    This reverts commit 07a91e04dc7c825347072ae6c05306a36d76e364.

--- 2652-sum-mult.rkt
+++ 2652-sum-mult.rkt
@@ -1,9 +1,12 @@
 #lang racket
 
 (define (sum-of-multiples n)
-  (for/sum ([n (inclusive-range 0 n)]
-            #:when (or (zero? (modulo n 3)) (zero? (modulo n 5)) (zero? (modulo n 7))))
-           n))
+  (for/sum ([n (inclusive-range 0 n)])
+           (cond
+             [(zero? (modulo n 3)) n]
+             [(zero? (modulo n 5)) n]
+             [(zero? (modulo n 7)) n]
+             [else 0])))
 
 (sum-of-multiples 7)
 (sum-of-multiples 10)
