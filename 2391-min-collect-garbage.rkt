b866632b87d86a445d5bac1052a11a3497017f93
ktunprasert <ktunprasert@outlook.com>
2025-06-16 23:46:38 +0100

    Revert "2391: claude got 30ms"
    This reverts commit bcd55dbe0b2b8aca1b5f0674ddc25ff344884113.

--- 2391-min-collect-garbage.rkt
+++ 2391-min-collect-garbage.rkt
@@ -1,19 +1,29 @@
 #lang racket
 
 (define (garbage-collection garbage travel)
-  ;; Single pass with true incremental prefix sum
-  (define-values (G-time P-time M-time total-garbage)
-    (for/fold ([G-time 0] [P-time 0] [M-time 0] [total-len 0] [prefix-sum 0]
-               #:result (values G-time P-time M-time total-len))
-              ([house garbage] [t (in-list (cons 0 travel))])
-      (let ([new-prefix (+ prefix-sum t)])
-        (values (if (string-contains? house "G") new-prefix G-time)
-                (if (string-contains? house "P") new-prefix P-time)
-                (if (string-contains? house "M") new-prefix M-time)
-                (+ total-len (string-length house))
-                new-prefix))))
+  ;; Single pass to find last indices and total garbage length
+  (define-values (Gi Pi Mi total-garbage)
+    (for/fold ([Gi #f] [Pi #f] [Mi #f] [total-len 0])
+              ([house garbage] [i (in-naturals)])
+      (values (if (string-contains? house "G") i Gi)
+              (if (string-contains? house "P") i Pi)
+              (if (string-contains? house "M") i Mi)
+              (+ total-len (string-length house)))))
 
-  (+ G-time P-time M-time total-garbage))
+  ;; Calculate travel times for each garbage type
+  ;; Build prefix sums for travel times
+  (define prefix-sums
+    (reverse (foldl (lambda (t acc)
+                      (cons (+ t (car acc)) acc))
+                    '(0)
+                    travel)))
+
+  (define travel-time
+    (+ (if Gi (list-ref prefix-sums Gi) 0)
+       (if Pi (list-ref prefix-sums Pi) 0)
+       (if Mi (list-ref prefix-sums Mi) 0)))
+
+  (+ travel-time total-garbage))
 
 ;; (garbage-collection (list "G" "P" "GP" "MGP") (list 2 4 3))
 (garbage-collection (list "G" "P" "GP" "GG") (list 2 4 3))
