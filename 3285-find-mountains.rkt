f7c608ccb556c4fb54eb6006beafde9d77386684
ktunprasert <ktunprasert@outlook.com>
2025-06-16 14:37:04 +0100

    3285: ok wash

--- 3285-find-mountains.rkt
+++ 3285-find-mountains.rkt
@@ -1,9 +1,8 @@
 #lang racket
 
 (define (stable-mountains height threshold)
-  (for/fold ([out empty])
-            ([i (in-range (sub1 (length height)))]
-             #:when (> (first (drop height i)) threshold))
-    (cons (add1 i) out)))
+  (for/list ([(n i) (in-indexed (drop-right height 1))]
+             #:when (> n threshold))
+    (add1 i)))
 
 (stable-mountains (list 1 2 3 4 5) 2)
