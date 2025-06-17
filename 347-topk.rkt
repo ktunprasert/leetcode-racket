4897cfc63f05f23d672a708faf379727465997f4
ktunprasert <ktunprasert@outlook.com>
2025-06-17 22:34:54 +0100

    347: welp 40ms faster

--- 347-topk.rkt
+++ 347-topk.rkt
@@ -1,27 +1,10 @@
 #lang racket
 
 (define (top-k-frequent nums k)
-  (cond
-    [(= k (length nums)) nums]
-    [else
-     (define nhash (make-hash))
-     (for ([n nums])
-       (hash-update! nhash n add1 0))
-
-     (let loop ([out empty]
-                [n (take (sort (hash-values nhash) >) k)])
-       (cond
-         [(= k (length out)) out]
-         [else
-          (begin0 (loop (cons (for/first ([(key v) nhash]
-                                          #:when (= v (first n)))
-                                (hash-remove! nhash key)
-                                key)
-                              out)
-                        (drop n 1))
-            )]))]))
+  (map first (take (sort (group-by identity nums) (lambda (a b) (> (length a) (length b)))) k)))
 
 (top-k-frequent '(1 1 1 2 2 3) 2) ; 1 2
 (top-k-frequent '(1) 1) ; 1
 (top-k-frequent '(3) 1) ; 1
 (top-k-frequent '(1 2) 2) ; 1
+(top-k-frequent '(-1 -2 -1 -2 -1 -2 -1 -1 9 -162931 96987 694873) 2) ; 1
