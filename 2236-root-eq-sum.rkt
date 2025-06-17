54ed812ac4f03e8049f4fada6b31bd9115e582f9
ktunprasert <ktunprasert@outlook.com>
2025-06-17 01:25:55 +0100

    2236: how is this more memory????

--- 2236-root-eq-sum.rkt
+++ 2236-root-eq-sum.rkt
@@ -3,18 +3,8 @@
 (require "bin-tree-helper.rkt")
 
 (define (check-tree root)
-  (if (false? root)
-      #f
-      (let ([total-sum (sum-all-nodes root)]
-            [root-val (tree-node-val root)])
-        (= root-val (- total-sum root-val)))))
-
-(define (sum-all-nodes node)
-  (cond
-    [(false? node) 0]
-    [else (+ (tree-node-val node)
-             (sum-all-nodes (tree-node-left node))
-             (sum-all-nodes (tree-node-right node)))]))
+  (= (tree-node-val root)
+     (+ (tree-node-val (tree-node-left root)) (tree-node-val (tree-node-right root)))))
 
 (define t1 (make-tree-node 5 (make-tree-node 2) (make-tree-node 3)))
 (define t2 (make-tree-node 1 (make-tree-node 2) (make-tree-node 3)))
