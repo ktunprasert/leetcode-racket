87cf122b7a417d1b502666ebb3c231b268c886a9
ktunprasert <ktunprasert@outlook.com>
2025-06-17 22:46:46 +0100

    347: 45ms

--- 347-topk.rkt
+++ 347-topk.rkt
@@ -1,7 +1,10 @@
 #lang racket
 
 (define (top-k-frequent nums k)
-  (map first (take (sort (group-by identity nums) (lambda (a b) (> (length a) (length b)))) k)))
+  (define ngroup (sort (group-by identity nums) (lambda (a b) (> (length a) (length b)))))
+  (for/list ([_ (in-range k)]
+             [ng ngroup])
+    (first ng)))
 
 (top-k-frequent '(1 1 1 2 2 3) 2) ; 1 2
 (top-k-frequent '(1) 1) ; 1
