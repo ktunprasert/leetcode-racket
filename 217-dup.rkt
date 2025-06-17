bb69bd45aeeb36e458698686c7816f7ba9355799
ktunprasert <ktunprasert@outlook.com>
2025-06-17 19:01:35 +0100

    217: ehh

--- 217-dup.rkt
+++ 217-dup.rkt
@@ -3,7 +3,7 @@
 (define (contains-duplicate nums)
   (define hsh (make-hash))
   (for/or ([n1 nums])
-    (if (hash-ref hsh n1 #f)
+    (if (hash-ref! hsh n1 #f)
         #t
         (begin0 #f
           (hash-set! hsh n1 #t)))))
