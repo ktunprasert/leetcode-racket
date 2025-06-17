56766504379535a4f031ca553bf2546f25b37926
ktunprasert <ktunprasert@outlook.com>
2025-06-17 19:00:39 +0100

    217: slower

--- 217-dup.rkt
+++ 217-dup.rkt
@@ -1,12 +1,11 @@
 #lang racket
 
 (define (contains-duplicate nums)
-  (define hsh (make-hash))
-  (for/or ([n1 nums])
-    (if (hash-ref hsh n1 #f)
-        #t
-        (begin0 #f
-          (hash-set! hsh n1 #t)))))
+  (let ([seen (mutable-set)])
+    (for/or ([num nums])
+      (cond
+        [(set-member? seen num) #t]
+        [else (set-add! seen num) #f]))))
 
 (contains-duplicate '(1 2 3))
 (contains-duplicate '(1 2 3 2))
