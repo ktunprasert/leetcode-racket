a5d196bc22077e667523d7680a143feb0341651f
ktunprasert <ktunprasert@outlook.com>
2025-06-17 20:01:15 +0100

    49: meh

--- 49-group-ana.rkt
+++ 49-group-ana.rkt
@@ -1,13 +1,17 @@
 #lang racket
 
- (define (group-anagrams strs)
-   (let ([groups (make-hash)])
-     (for ([str strs])
-       (let ([key (list->string (sort (string->list str) char<?))])
-         (hash-update! groups key
-                       (lambda (lst) (cons str lst))
-                       '())))
-     (hash-values groups)))
+(define memoize (make-hash))
+(define (make-anagram-key s)
+  (if (hash-has-key? memoize s)
+      (hash-ref memoize s)
+      (sort (string->list s) char<?)))
+
+(define (group-anagrams strs)
+  (let ([groups (make-hash)])
+    (for ([str strs])
+      (let ([key (sort (string->list str) char<?)])
+        (hash-update! groups key (lambda (lst) (cons str lst)) '())))
+    (hash-values groups)))
 
 (group-anagrams '("eat" "tea" "tan" "ate" "nat" "bat"))
 (group-anagrams '("" "c" "d"))
