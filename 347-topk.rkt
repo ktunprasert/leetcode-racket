6a52c63365b982dfaf6898c132d7b80c918c428b
ktunprasert <ktunprasert@outlook.com>
2025-06-17 22:44:52 +0100

    347: 150ms

--- 347-topk.rkt
+++ 347-topk.rkt
@@ -1,7 +1,14 @@
 #lang racket
 
 (define (top-k-frequent nums k)
-  (map first (take (sort (group-by identity nums) (lambda (a b) (> (length a) (length b)))) k)))
+  (define ns (group-by identity nums))
+  (define (take-while lst [out empty])
+    (cond
+      [(= k (length out)) out]
+      [else
+       (define ls (argmax length lst))
+       (take-while (remove ls lst) (cons (first ls) out))]))
+  (take-while ns))
 
 (top-k-frequent '(1 1 1 2 2 3) 2) ; 1 2
 (top-k-frequent '(1) 1) ; 1
