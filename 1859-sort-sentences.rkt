2e27815c20a5fb3a984be61b50c1f11d33800ef0
ktunprasert <ktunprasert@outlook.com>
2025-06-23 21:10:19 +0100

    1859: list only is 170ms

--- 1859-sort-sentences.rkt
+++ 1859-sort-sentences.rkt
@@ -2,10 +2,10 @@
 
 (define (sort-sentence s)
   (define slist (string-split s))
-  (define smap (make-hash))
+  (define out (make-list 10 ""))
   (for ([s slist])
-    (define key (string-ref s (sub1 (string-length s))))
-    (hash-set! smap key (substring s 0 (sub1 (string-length s)))))
-  (string-join (hash-values smap #t)))
+    (define key (- (char->integer (string-ref s (sub1 (string-length s)))) 48))
+    (set! out (list-set out key (substring s 0 (sub1 (string-length s))))))
+  (string-join (filter-not (curry eq? "") out)))
 
 (sort-sentence "is2 sentence4 This1 a3")
