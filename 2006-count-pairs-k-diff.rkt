34a1e9e06acb126453c43cfae00901fad3913aa1
ktunprasert <ktunprasert@outlook.com>
2025-06-24 00:07:25 +0100

    2006: diff syntax

--- 2006-count-pairs-k-diff.rkt
+++ 2006-count-pairs-k-diff.rkt
@@ -1,7 +1,6 @@
 #lang racket
 
 (define (count-k-difference nums k)
-  (for/sum ([(n1 i) (in-indexed nums)])
-           (for/sum ([n2 (drop nums (add1 i))] #:when (= k (abs (- n1 n2)))) 1)))
+  (for*/sum ([(n1 i) (in-indexed nums)] [n2 (drop nums (add1 i))] #:when (= k (abs (- n1 n2)))) 1))
 
 (count-k-difference (list 1 2 2 1) 1)
