ae963bab1cf788b76847438aa1978df0b70cc4c2
ktunprasert <ktunprasert@outlook.com>
2025-06-23 23:40:02 +0100

    1859: slow slow slow

--- 1859-sort-sentences.rkt
+++ 1859-sort-sentences.rkt
@@ -1,13 +1,16 @@
 #lang racket
 
 (define (sort-sentence s)
-  (define slist (string-split s))
-  (string-join (map car
-                    (sort (map (lambda (s)
-                                 (cons (substring s 0 (sub1 (string-length s)))
-                                       (string-ref s (sub1 (string-length s)))))
-                               slist)
-                          #:key cdr
-                          char<?))))
+  (define schars (string->list s))
+  (define out (make-hash))
+  (for/fold ([word empty]
+             #:result (hash-set! out (first word) (reverse (cdr word))))
+            ([c schars])
+    (if (char=? c #\space)
+        (begin
+          (hash-set! out (first word) (reverse (cdr word)))
+          empty)
+        (cons c word)))
+  (list->string (flatten (add-between (hash-values out #t) #\space))))
 
 (sort-sentence "is2 sentence4 This1 a3")
