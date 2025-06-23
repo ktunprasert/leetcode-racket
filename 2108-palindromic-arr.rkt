ef33ca5d41188c4fc4a28afdcabee94f78d12793
ktunprasert <ktunprasert@outlook.com>
2025-06-23 19:02:37 +0100

    2108: 11ms

---
+++ 2108-palindromic-arr.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+(define (first-palindrome words)
+  (or (for/or ([w words])
+        (if (for/and ([c w]
+                      [i (in-naturals)])
+              (char=? (string-ref w (- (sub1 (string-length w)) i)) c))
+            w
+            #f))
+      ""))
+
+(first-palindrome '("abc" "def" "racecar"))
+(first-palindrome '("abc" "def"))
