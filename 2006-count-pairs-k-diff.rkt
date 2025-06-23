1fb8ee4ed498122ac7fde86d2a774894c737ee7d
ktunprasert <ktunprasert@outlook.com>
2025-06-24 00:05:07 +0100

    2006: same

--- 2006-count-pairs-k-diff.rkt
+++ 2006-count-pairs-k-diff.rkt
@@ -2,6 +2,6 @@
 
 (define (count-k-difference nums k)
   (for/sum ([(n1 i) (in-indexed nums)])
-           (for/sum ([n2 (drop nums (add1 i))]) (if (= k (abs (- n2 n1))) 1 0))))
+           (for/sum ([n2 (drop nums (add1 i))] #:when (= k (abs (- n1 n2)))) 1)))
 
 (count-k-difference (list 1 2 2 1) 1)
