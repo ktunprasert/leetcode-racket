65b301a874839ff6ea6f13d47e5b89e197b937de
ktunprasert <ktunprasert@outlook.com>
2025-06-24 16:53:03 +0100

    709: fine ill do it myself

--- 709-lowercase.rkt
+++ 709-lowercase.rkt
@@ -1,3 +1,9 @@
 #lang racket
 
-(define to-lower-case string-downcase)
+(define (to-lower-case s)
+  (list->string (for/list ([c s])
+                  (if (and (char>=? c #\A) (char<=? c #\Z))
+                      (integer->char (+ (char->integer c) 32))
+                      c))))
+
+(to-lower-case "Hello World!")
