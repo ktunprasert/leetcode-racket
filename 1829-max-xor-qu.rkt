6c8cc011b635429a0453488d4e035b98d89edfbe
ktunprasert <ktunprasert@outlook.com>
2025-06-24 15:20:01 +0100

    1829: still 21ms

--- 1829-max-xor-qu.rkt
+++ 1829-max-xor-qu.rkt
@@ -2,8 +2,10 @@
 
 (define (get-maximum-xor nums maximumBit)
   (define target (sub1 (expt 2 maximumBit)))
-  (for/foldr ([out empty] [prefix 0] #:result out)
-             ([n2 (reverse nums)])
-             (values (cons (bitwise-xor target prefix n2) out) (bitwise-xor prefix n2))))
+  (for/fold ([out empty]
+             [prefix 0]
+             #:result out)
+            ([n nums])
+    (values (cons (bitwise-xor target prefix n) out) (bitwise-xor prefix n))))
 
 (get-maximum-xor '[0 1 1 3] 2)
