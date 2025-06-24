342659e26a829ebfaeb87290c5e7c6568dcb6a75
ktunprasert <ktunprasert@outlook.com>
2025-06-24 16:44:09 +0100

    Revert "2176: hash slower"
    This reverts commit 9d03996f1211172295208b1e903c4aef14d2ea84.

--- 2176.rkt
+++ 2176.rkt
@@ -1,14 +1,8 @@
 #lang racket
 
 (define (count-pairs nums k)
-  (define nmap (make-hash))
-  (for ([(n i) (in-indexed nums)])
-    (hash-update! nmap n (curry cons i) empty))
-  (for*/sum ([lst (in-hash-values nmap)] [combs (in-combinations lst 2)]
-                                         #:when (> (length lst) 1)
-                                         #:when (zero? (modulo (apply * combs) k)))
-            1))
+  (for*/sum ([(n1 i) (in-indexed nums)] [(n2 j) (in-indexed (drop nums (+ 1 i)))] #:when (= n1 n2))
+            (if (zero? (modulo (* i (+ i j 1)) k)) 1 0)))
 
 (count-pairs (list 3 1 2 2 2 1 3) 2)
-(count-pairs (list 1 2 3 4) 1)
-;; (combinations (list 1 2 3) 2)
+;; (count-pairs (list 1 2 3 4) 1)
