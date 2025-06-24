15416d4d57c0f15a73e414c2587fc459f74feb62
ktunprasert <ktunprasert@outlook.com>
2025-06-24 19:23:34 +0100

    Revert "2965: ok slower"
    This reverts commit 975716005f003f8d291d2e83dbd97fb65bb5e900.

--- 2965.rkt
+++ 2965.rkt
@@ -1,12 +1,13 @@
 #lang racket
 
-(define (sum n)
-  (/ (* n (add1 n)) 2))
-
 (define (find-missing-and-repeated-values grid)
   (define ls (sort (flatten grid) <))
   (define duped (check-duplicates ls))
-  (list duped (- (sum (length ls)) (- (apply + ls) duped))))
+  (for/fold ([ns 0]
+             [is 0]
+             #:result (list duped (abs (- ns is duped))))
+            ([(n i) (in-indexed ls)])
+    (values (+ n ns) (+ i is 1))))
 
 (find-missing-and-repeated-values '((1 3) (2 2)))
 (find-missing-and-repeated-values '[[9 1 7] [8 9 2] [3 4 6]])
