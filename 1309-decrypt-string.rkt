8341c8222c330f1fac85775605466559aec412f4
ktunprasert <ktunprasert@outlook.com>
2025-06-24 23:19:13 +0100

    1309: lexer goated, do block goated

---
+++ 1309-decrypt-string.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(require parser-tools/lex)
+
+(define the-lexer
+  (lexer [(eof) eof]
+         [(concatenation (repetition 2 2 numeric) "#")
+          (string->number (substring lexeme 0 (sub1 (string-length lexeme))))]
+         [(repetition 1 1 numeric) (string->number lexeme)]
+         [any-char (the-lexer input-port)]))
+
+(define (freq-alphabets s)
+  (define sport (open-input-string s))
+  (let ([numbers (do ([n (the-lexer sport) (the-lexer sport)] [ns empty])
+                     ((eof-object? n) (reverse ns))
+                     (set! ns (cons n ns)))])
+    (list->string (map integer->char (map (curry + 96) numbers)) )))
+
+(freq-alphabets "123456")
+(freq-alphabets "10#")
