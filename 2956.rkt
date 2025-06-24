88b1990a96ddd47bc2fc43f3ea11bf812d00f93a
ktunprasert <ktunprasert@outlook.com>
2025-06-24 17:31:40 +0100

    2956: sssssssssssslow

--- 2956.rkt
+++ 2956.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
 (define (find-intersection-values nums1 nums2)
-  (list (count (curry index-of nums2) nums1) (count (curry index-of nums1) nums2)))
+  (list (count (curryr member nums2) nums1) (count (curryr member nums1) nums2)))
 
 (find-intersection-values (list 2 3 2) (list 1 2))
