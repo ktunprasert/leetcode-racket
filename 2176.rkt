8c12f87ca208afb7a29d834f5fa2140bafc69c56
ktunprasert <ktunprasert@outlook.com>
2025-06-24 16:46:05 +0100

    2176: vector FAST

--- 2176.rkt
+++ 2176.rkt
@@ -1,8 +1,13 @@
 #lang racket
 
 (define (count-pairs nums k)
-  (for*/sum ([(n1 i) (in-indexed nums)] [(n2 j) (in-indexed (drop nums (+ 1 i)))] #:when (= n1 n2))
-            (if (zero? (modulo (* i (+ i j 1)) k)) 1 0)))
+  (define vec (list->vector nums))
+  (define len (vector-length vec))
+  (for/sum ([i (in-range len)])
+    (for/sum ([j (in-range (+ i 1) len)])
+      (if (and (= (vector-ref vec i) (vector-ref vec j))
+               (zero? (modulo (* i j) k)))
+          1 0))))
 
 (count-pairs (list 3 1 2 2 2 1 3) 2)
 ;; (count-pairs (list 1 2 3 4) 1)
