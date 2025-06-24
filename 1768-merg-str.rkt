203f63500186daacc9e7d1929e12208e98d662ed
ktunprasert <ktunprasert@outlook.com>
2025-06-24 14:33:22 +0100

    1768: faster

--- 1768-merg-str.rkt
+++ 1768-merg-str.rkt
@@ -3,15 +3,15 @@
 (define (merge-alternately word1 word2)
   (define sl1 (string-length word1))
   (define sl2 (string-length word2))
-
-  (string-append (list->string
-                        (flatten (for/list ([w1 word1]
-                                            [w2 word2])
-                                   (list w1 w2))))
-                 (cond
-                   [(= sl1 sl2) ""]
-                   [(> sl1 sl2) (substring word1 sl2)]
-                   [(> sl2 sl1) (substring word2 sl1)])))
+  (define w1 (string->list word1))
+  (define w2 (string->list word2))
+  (list->string (flatten (list (for/list ([w1 w1]
+                                          [w2 w2])
+                                 (list w1 w2))
+                               (cond
+                                 [(> sl1 sl2) (drop w1 sl2)]
+                                 [(> sl2 sl1) (drop w2 sl1)]
+                                 [else empty])))))
 
 (merge-alternately "abc" "123")
 (merge-alternately "abcdef" "123")
