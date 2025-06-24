70b2a6e86b440267e2f76abdda75a5d57a113639
ktunprasert <ktunprasert@outlook.com>
2025-06-24 16:47:47 +0100

    2176: vector FAST SINGLE for*/sum

--- 2176.rkt
+++ 2176.rkt
@@ -3,11 +3,10 @@
 (define (count-pairs nums k)
   (define vec (list->vector nums))
   (define len (vector-length vec))
-  (for/sum ([i (in-range len)])
-    (for/sum ([j (in-range (+ i 1) len)])
-      (if (and (= (vector-ref vec i) (vector-ref vec j))
-               (zero? (modulo (* i j) k)))
-          1 0))))
+  (for*/sum ([i (in-range len)] [j (in-range (+ i 1) len)]
+                                #:when (= (vector-ref vec i) (vector-ref vec j))
+                                #:when (zero? (modulo (* i j) k)))
+            1))
 
 (count-pairs (list 3 1 2 2 2 1 3) 2)
 ;; (count-pairs (list 1 2 3 4) 1)
