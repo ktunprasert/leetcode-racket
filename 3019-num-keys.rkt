aef5c43a9a2d5f4b0c50532069c97409210cb4ab
ktunprasert <ktunprasert@outlook.com>
2025-06-24 23:46:27 +0100

    3019: ok

--- 3019-num-keys.rkt
+++ 3019-num-keys.rkt
@@ -2,6 +2,12 @@
 
 (define/contract (count-key-changes s)
   (-> string? exact-integer?)
-  (sub1 (set-count (list->set (string->list (string-downcase s))))))
+  (define slow (string-downcase s))
+  (let loop ([s (string->list slow)]
+             [total 0])
+    (cond
+      [(empty? s) total]
+      [(= 1 (length s)) total]
+      [else (loop (cdr s) (+ total (if (not (char=? (car s) (cadr s))) 1 0)))])))
 
-(count-key-changes "abc")
+(count-key-changes "Bbc")
