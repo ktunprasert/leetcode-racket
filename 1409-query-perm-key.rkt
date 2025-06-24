e42ddda9a357b4c4f5e4727bec64a3efa38fd9a9
ktunprasert <ktunprasert@outlook.com>
2025-06-24 16:10:05 +0100

    1409: meh

--- 1409-query-perm-key.rkt
+++ 1409-query-perm-key.rkt
@@ -4,7 +4,6 @@
   (define plist (inclusive-range 1 m))
   (for/list ([q queries])
     (begin0 (index-of plist q)
-      (set! plist (remove q plist))
-      (set! plist (cons q plist)))))
+      (set! plist (cons q (remove q plist))))))
 
 (process-queries (list 3 1 2 1) 5)
