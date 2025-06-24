96624d003a387a45012e16a1290219ed9e399057
ktunprasert <ktunprasert@outlook.com>
2025-06-24 18:54:07 +0100

    1588: one for*sum 66ms

--- 1588-sum-odd-arr.rkt
+++ 1588-sum-odd-arr.rkt
@@ -2,10 +2,10 @@
 
 (define (sum-odd-length-subarrays arr)
   (define slices (filter odd? (inclusive-range 1 (length arr))))
-  (for*/sum ([s slices])
-            (for/sum ([i (in-inclusive-range 0 (- (length arr) s))])
-                     ;; (println (list s (take (drop arr i) s )))
-                     (apply + (take (drop arr i) s)))))
+  (for*/sum ([s slices]
+            [i (in-inclusive-range 0 (- (length arr) s))])
+            ;; (println (list s (take (drop arr i) s )))
+            (apply + (take (drop arr i) s))))
 
 ;; (sum-odd-length-subarrays (list 1 4 2 5 3))
 ;; (sum-odd-length-subarrays (list 1 2))
