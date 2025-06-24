61e29efd9104f36b1271e339714c946d0293287d
ktunprasert <ktunprasert@outlook.com>
2025-06-24 17:36:04 +0100

    2956: set fast

--- 2956.rkt
+++ 2956.rkt
@@ -1,6 +1,8 @@
 #lang racket
 
 (define (find-intersection-values nums1 nums2)
-  (list (count (curry index-of nums2) nums1) (count (curry index-of nums1) nums2)))
+  (define set1 (list->set nums1))
+  (define set2 (list->set nums2))
+  (list (count (curry set-member? set2) nums1) (count (curry set-member? set1) nums2)))
 
 (find-intersection-values (list 2 3 2) (list 1 2))
