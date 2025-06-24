922b121930002c623a74ae093c75ad587ceeb14d
ktunprasert <ktunprasert@outlook.com>
2025-06-24 18:59:45 +0100

    1588: wash

--- 1588-sum-odd-arr.rkt
+++ 1588-sum-odd-arr.rkt
@@ -4,7 +4,6 @@
   (define arr-len (length arr))
   (define slices (inclusive-range 1 (length arr) 2))
   (for*/sum ([s slices] [i (in-inclusive-range 0 (- arr-len s))])
-            ;; (println (list s (take (drop arr i) s )))
             (apply + (take (drop arr i) s))))
 
 ;; (sum-odd-length-subarrays (list 1 4 2 5 3))
