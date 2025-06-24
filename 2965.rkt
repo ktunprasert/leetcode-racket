975716005f003f8d291d2e83dbd97fb65bb5e900
ktunprasert <ktunprasert@outlook.com>
2025-06-24 19:23:31 +0100

    2965: ok slower

--- 2965.rkt
+++ 2965.rkt
@@ -1,13 +1,12 @@
 #lang racket
 
+(define (sum n)
+  (/ (* n (add1 n)) 2))
+
 (define (find-missing-and-repeated-values grid)
   (define ls (sort (flatten grid) <))
   (define duped (check-duplicates ls))
-  (for/fold ([ns 0]
-             [is 0]
-             #:result (list duped (abs (- ns is duped))))
-            ([(n i) (in-indexed ls)])
-    (values (+ n ns) (+ i is 1))))
+  (list duped (- (sum (length ls)) (- (apply + ls) duped))))
 
 (find-missing-and-repeated-values '((1 3) (2 2)))
 (find-missing-and-repeated-values '[[9 1 7] [8 9 2] [3 4 6]])
