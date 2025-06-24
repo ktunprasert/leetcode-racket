433a0968d40aff5bc4edd5af5304b782137bed8b
ktunprasert <ktunprasert@outlook.com>
2025-06-24 18:21:15 +0100

    1572: readable

--- 1572-matrix-diag-sum.rkt
+++ 1572-matrix-diag-sum.rkt
@@ -2,6 +2,8 @@
 
 (define (diagonal-sum mat)
   (define cols (length (first mat)))
+  (define (grid-at pair)
+    ((compose (curryr list-ref (car pair)) (curryr list-ref (cdr pair))) mat))
   (let loop ([offset 0]
              [n 0]
              [sum 0])
@@ -10,13 +12,13 @@
       [else
        (define left (cons n n))
        (define right (cons n (- cols offset 1)))
-       (define left-n (list-ref (list-ref mat (car left)) (cdr left)))
-       (define right-n (list-ref (list-ref mat (car right)) (cdr right)))
+       (define left-n (grid-at left))
+       (define right-n (grid-at right))
        (when (equal? left right)
          (set! left-n 0))
        (loop (add1 offset) (add1 n) (+ sum left-n right-n))])))
 
-(diagonal-sum '((5)))
-;; (diagonal-sum '((1 2 3) (4 5 6) (7 8 9)))
+;; (diagonal-sum '((5)))
+(diagonal-sum '((1 2 3) (4 5 6) (7 8 9)))
 ;; (diagonal-sum '((1 2) (3 4)))
 ;; (diagonal-sum '((1 2 3 4) (5 6 7 8) (9 10 11 12) (13 14 15 16)))
