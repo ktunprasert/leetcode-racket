3f9cd451611f605e1b95a4148358492bdc53d7f9
ktunprasert <ktunprasert@outlook.com>
2025-06-24 15:26:20 +0100

    1829: prefix include target - but it's SLOWER???

--- 1829-max-xor-qu.rkt
+++ 1829-max-xor-qu.rkt
@@ -2,9 +2,9 @@
 
 (define (get-maximum-xor nums maximumBit)
   (define target (sub1 (expt 2 maximumBit)))
-  (define prefix 0)
+  (define prefix target)
   (reverse (for/list ([n nums])
-             (begin0 (bitwise-xor target prefix n)
+             (begin0 (bitwise-xor prefix n)
                (set! prefix (bitwise-xor prefix n))))))
 
 (get-maximum-xor '[0 1 1 3] 2)
