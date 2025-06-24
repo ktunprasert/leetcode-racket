8cdd303008c3d89d188ef992e6a71f9583aa76c3
ktunprasert <ktunprasert@outlook.com>
2025-06-24 19:02:22 +0100

    1588: meh

--- 1588-sum-odd-arr.rkt
+++ 1588-sum-odd-arr.rkt
@@ -3,8 +3,11 @@
 (define (sum-odd-length-subarrays arr)
   (define arr-len (length arr))
   (define slices (inclusive-range 1 (length arr) 2))
-  (for*/sum ([s slices] [i (in-inclusive-range 0 (- arr-len s))])
-            (apply + (take (drop arr i) s))))
+  (foldl (λ (l acc) (+ acc (apply + l)))
+         0
+         (for*/list ([s slices]
+                     [i (in-inclusive-range 0 (- arr-len s))])
+           (take (drop arr i) s))))
 
 ;; (sum-odd-length-subarrays (list 1 4 2 5 3))
 ;; (sum-odd-length-subarrays (list 1 2))
