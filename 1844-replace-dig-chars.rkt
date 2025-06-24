023bf64abddee29a68cca18f7161d885a7dbe9c1
ktunprasert <ktunprasert@outlook.com>
2025-06-24 14:50:18 +0100

    1844: working

--- 1844-replace-dig-chars.rkt
+++ 1844-replace-dig-chars.rkt
@@ -2,9 +2,14 @@
 
 (define (replace-digits s)
   (list->string (flatten (for/list ([dubs (in-slice 2 s)])
-                           (match-define (list a b) dubs)
-                           (list a
-                                 (integer->char (+ (char->integer a)
-                                                   (- (char->integer b) (char->integer #\0)))))))))
+                           (if (= (length dubs) 2)
+                               (begin
+                                 (match-let ([(list a b) dubs])
+                                   (list a
+                                         (integer->char (+ (char->integer a)
+                                                           (- (char->integer b)
+                                                              (char->integer #\0)))))))
+                               (car dubs))))))
 
 (replace-digits "a1c1e1") ; abcdef
+(replace-digits "a1c1e1z") ; abcdefz
