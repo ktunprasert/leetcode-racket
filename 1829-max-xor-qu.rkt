9d46852bb6afa29ef8335c6a2858788b892749ff
ktunprasert <ktunprasert@outlook.com>
2025-06-24 15:21:24 +0100

    1829: for/list faster

--- 1829-max-xor-qu.rkt
+++ 1829-max-xor-qu.rkt
@@ -2,10 +2,9 @@
 
 (define (get-maximum-xor nums maximumBit)
   (define target (sub1 (expt 2 maximumBit)))
-  (for/fold ([out empty]
-             [prefix 0]
-             #:result out)
-            ([n nums])
-    (values (cons (bitwise-xor target prefix n) out) (bitwise-xor prefix n))))
+  (define prefix 0)
+  (reverse (for/list ([n nums])
+             (begin0 (bitwise-xor target prefix n)
+               (set! prefix (bitwise-xor prefix n))))))
 
 (get-maximum-xor '[0 1 1 3] 2)
