bfcc59906f86761dc6437c511cc71d821ba9df6c
ktunprasert <ktunprasert@outlook.com>
2025-06-25 13:21:07 +0100

    1295: need precision control

--- 1295-even-num-digs.rkt
+++ 1295-even-num-digs.rkt
@@ -2,7 +2,8 @@
 
 (define/contract (find-numbers nums)
   (-> (listof exact-integer?) exact-integer?)
-  (count (λ (n) (even? (add1 (floor (log n 10))))) nums))
+  (count (λ (n) (even? (+ 1 (floor (+ 1e-5 (log n 10)))))) nums))
 
 (find-numbers '[12 345 2 6 7896])
 (find-numbers '[300])
+(find-numbers '[1000])
