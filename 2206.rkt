fa789528c301589efb3ccb9de2df18bfbcd7ea7b
ktunprasert <ktunprasert@outlook.com>
2025-06-25 15:02:44 +0100

    2206: ok still

--- 2206.rkt
+++ 2206.rkt
@@ -3,14 +3,10 @@
 (define (divide-array nums)
   (define expected-pairs (quotient (length nums) 2))
   (define sorted (list->vector (sort nums <)))
-
+  (define n-len (length nums))
   (define (ref i)
     (vector-ref sorted i))
-
-  (= expected-pairs
-     (for/sum ([i (in-range 0 (length nums) 2)] [j (in-range 1 (length nums) 2)]
-                                                #:break ((negate =) (ref i) (ref j)))
-              1)))
+  (= expected-pairs (count (λ (i j) (= (ref i) (ref j))) (range 0 n-len 2) (range 1 n-len 2))))
 
 (divide-array '[2 2 3 3])
 (divide-array '[1 2 3 4])
