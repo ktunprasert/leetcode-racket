fadf6e285761df7a4aa99bf26e406573bad823cb
ktunprasert <ktunprasert@outlook.com>
2025-06-25 14:41:40 +0100

    1652: not any faster

--- 1652-defuse-bomb.rkt
+++ 1652-defuse-bomb.rkt
@@ -5,14 +5,16 @@
   [(_ _)
    (define code-vec (list->vector code))
    (define code-len (vector-length code-vec))
-   (define (cyclic-range i)
+   (define cyclic-range
      (if (negative? k)
-         (build-list (abs k) (compose (curryr modulo code-len) (curryr + k i)))
-         (build-list k (compose (curryr modulo code-len) (curryr + i 1)))))
+         (build-list (abs k) (curryr + k))
+         (build-list k (curryr + 1))))
    (for/list ([i (in-range code-len)])
-     (foldl (λ (i acc) (+ acc (vector-ref code-vec i))) 0 (cyclic-range i)))])
+     (foldl (λ (i acc) (+ acc (vector-ref code-vec i)))
+            0
+            (map (compose (curryr modulo code-len) (curry + i)) cyclic-range)))])
 
 ;; (decrypt '(1 2 3 4 5) 0)
-(decrypt '(1 2 3 4 5) 1)
+;; (decrypt '(1 2 3 4 5) 1)
 ;; (decrypt '(5 7 1 4) 3)
-;; (decrypt '(2 4 9 3) -2)
+(decrypt '(2 4 9 3) -2)
