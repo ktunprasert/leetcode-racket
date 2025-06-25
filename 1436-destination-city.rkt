dbeb5991b7fb915077ef0ade456790f89d052620
ktunprasert <ktunprasert@outlook.com>
2025-06-25 13:04:16 +0100

    1436: ok easy

---
+++ 1436-destination-city.rkt
@@ -0,0 +1,10 @@
+#lang racket
+
+(define (dest-city paths)
+  (define city-map
+    (for/hash ([path paths])
+      (apply values path)))
+  (do ([city (second (car paths)) (hash-ref city-map city #f)])
+      ((not (hash-has-key? city-map city)) city)))
+
+(dest-city '[["London" "New York"] ["New York" "Lima"] ["Lima" "Sao Paulo"]])
