3b44afd6051738acac67e54ee8ad06a2e0185ac4
ktunprasert <ktunprasert@outlook.com>
2025-06-25 13:15:04 +0100

    1295: use log10

--- 1295-even-num-digs.rkt
+++ 1295-even-num-digs.rkt
@@ -1,12 +1,8 @@
 #lang racket
 
-(define (num-digits n [digits 0])
-  (cond
-    [(zero? n) digits]
-    [else (num-digits (quotient n 10) (add1 digits))]))
-
 (define/contract (find-numbers nums)
   (-> (listof exact-integer?) exact-integer?)
-  (count (compose even? num-digits) nums))
+  (count (λ (n) (even? (add1 (floor (log n 10))))) nums))
 
 (find-numbers '[12 345 2 6 7896])
+(find-numbers '[300])
