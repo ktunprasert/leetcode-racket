37533c4e02a8311b5ac3a3db91554cf51de43d94
ktunprasert <ktunprasert@outlook.com>
2025-06-25 18:45:38 +0100

    2094: cooking

--- 2094-find-3-dig-even.rkt
+++ 2094-find-3-dig-even.rkt
@@ -1,15 +1,27 @@
 #lang racket
 
 (define (find-even-numbers digits)
-  (define (dig-sum a b c)
-    (+ (* 100 a) (* 10 b) c))
-  (sort (set->list (for*/set ([a digits]
-                              [b (remove a digits)]
-                              [c (remove b (remove a digits))]
-                              #:when (even? c)
-                              #:unless (zero? a))
-                     (dig-sum a b c)))
-        <))
+  (define freq (make-vector 10 0))
+  (for ([d digits])
+    (vector-set! freq d (add1 (vector-ref freq d))))
 
-;; (find-even-numbers '(2 1 3 0))
-(find-even-numbers '(2 2 8 8 2))
+  (define (freq-update i fn)
+    (vector-set! freq i (fn (vector-ref freq i))))
+
+  (println freq)
+
+  (for*/list ([a (in-range 1 10)]
+              [b 10]
+              [c (in-range 0 10 2)]
+              #:when (> (vector-ref freq a) 0)
+              #:when (> (vector-ref freq b) 0)
+              #:when (> (vector-ref freq c) 0))
+    (begin0 (list a b c)
+      (freq-update b add1)
+      (freq-update a add1)
+      )
+    )
+  )
+
+(find-even-numbers '(2 1 3 0))
+;; (find-even-numbers '(2 2 8 8 2))
