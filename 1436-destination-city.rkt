9e2391986eed1699fd55fe452ba951b5f374e6ab
ktunprasert <ktunprasert@outlook.com>
2025-06-25 13:05:06 +0100

    1436: negate

--- 1436-destination-city.rkt
+++ 1436-destination-city.rkt
@@ -5,6 +5,6 @@
     (for/hash ([path paths])
       (apply values path)))
   (do ([city (second (car paths)) (hash-ref city-map city #f)])
-      ((not (hash-has-key? city-map city)) city)))
+      (((negate hash-has-key?) city-map city) city)))
 
 (dest-city '[["London" "New York"] ["New York" "Lima"] ["Lima" "Sao Paulo"]])
