10d67ee664f9552efdbbe0a498fb96db91210037
ktunprasert <ktunprasert@outlook.com>
2025-06-25 19:07:59 +0100

    2094: slight washing?

--- 2094-find-3-dig-even.rkt
+++ 2094-find-3-dig-even.rkt
@@ -4,16 +4,18 @@
   (define freq (make-vector 10 0))
   (for ([d digits])
     (vector-set! freq d (add1 (vector-ref freq d))))
+  (define (freq-ref n)
+    (vector-ref freq n))
   (for*/list ([a (in-range 1 10)]
               [b 10]
               [c (in-range 0 10 2)]
-              #:when (> (vector-ref freq a) 0)
-              #:when (> (vector-ref freq b) 0)
-              #:when (> (vector-ref freq c) 0)
-              #:unless (and (= a b c) (not (> (vector-ref freq a) 2)))
-              #:unless (and (= a b) (not (> (vector-ref freq a) 1)))
-              #:unless (and (= a c) (not (> (vector-ref freq a) 1)))
-              #:unless (and (= b c) (not (> (vector-ref freq b) 1))))
+              #:when (> (freq-ref a) 0)
+              #:when (> (freq-ref b) 0)
+              #:when (> (freq-ref c) 0)
+              #:unless (and (= a b c) ((negate >) (freq-ref a) 2))
+              #:unless (and (= a b) ((negate >) (freq-ref a) 1))
+              #:unless (and (= a c) ((negate >) (freq-ref a) 1))
+              #:unless (and (= b c) ((negate >) (freq-ref b) 1)))
     (+ (* 100 a) (* 10 b) c)))
 
 (find-even-numbers '(2 1 3 0))
