ff9f7a602089aebf066c7a3248876eeaf8afdd11
ktunprasert <ktunprasert@outlook.com>
2025-06-25 15:44:02 +0100

    1748: filter-map

--- 1748-sum-uniq-elements.rkt
+++ 1748-sum-uniq-elements.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 (define (sum-of-unique nums)
-  (apply + (flatten (filter (compose (curry = 1) length) (group-by identity nums)))))
+  (apply + (filter-map (λ (ls) (and (= 1 (length ls)) (first ls))) (group-by identity nums))))
 
 (sum-of-unique '(1 1 1 1 1)) ; => 0
 (sum-of-unique '(1 2 3 4 5)) ; => 15
