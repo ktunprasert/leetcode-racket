e273caf8c20a74a24c1434dd6ecadbfe629900f8
ktunprasert <ktunprasert@outlook.com>
2025-06-25 19:04:06 +0100

    2094: GIGA GUARD

--- 2094-find-3-dig-even.rkt
+++ 2094-find-3-dig-even.rkt
@@ -4,24 +4,17 @@
   (define freq (make-vector 10 0))
   (for ([d digits])
     (vector-set! freq d (add1 (vector-ref freq d))))
-
-  (define (freq-update i fn)
-    (vector-set! freq i (fn (vector-ref freq i))))
-
-  (println freq)
-
   (for*/list ([a (in-range 1 10)]
               [b 10]
               [c (in-range 0 10 2)]
               #:when (> (vector-ref freq a) 0)
               #:when (> (vector-ref freq b) 0)
-              #:when (> (vector-ref freq c) 0))
-    (begin0 (list a b c)
-      (freq-update b add1)
-      (freq-update a add1)
-      )
-    )
-  )
+              #:when (> (vector-ref freq c) 0)
+              #:unless (and (= a b c) (not (> (vector-ref freq a) 2)))
+              #:unless (and (= a b) (not (> (vector-ref freq a) 1)))
+              #:unless (and (= a c) (not (> (vector-ref freq a) 1)))
+              #:unless (and (= b c) (not (> (vector-ref freq b) 1))))
+    (+ (* 100 a) (* 10 b) c)))
 
 (find-even-numbers '(2 1 3 0))
-;; (find-even-numbers '(2 2 8 8 2))
+(find-even-numbers '(2 2 8 8 2))
