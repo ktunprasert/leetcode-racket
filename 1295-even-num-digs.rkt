7b74d4bd3a7beec47a6d655e756f18a583570822
ktunprasert <ktunprasert@outlook.com>
2025-06-25 13:23:47 +0100

    1295: is this cheating

--- 1295-even-num-digs.rkt
+++ 1295-even-num-digs.rkt
@@ -1,8 +1,17 @@
 #lang racket
 
+(define (is-even-digits? n)
+  (cond
+    [(< n 10e0) #f]
+    [(< n 10e1) #t]
+    [(< n 10e2) #f]
+    [(< n 10e3) #t]
+    [(< n 10e4) #f]
+    [else #t]))
+
 (define/contract (find-numbers nums)
   (-> (listof exact-integer?) exact-integer?)
-  (count (λ (n) (even? (+ 1 (floor (+ 1e-5 (log n 10)))))) nums))
+  (count is-even-digits? nums))
 
 (find-numbers '[12 345 2 6 7896])
 (find-numbers '[300])
