7d92fa0503ade17eaa6a7888f1fb73bf08ec03c7
ktunprasert <ktunprasert@outlook.com>
2025-06-26 22:49:08 +0100

    2215: LMAO 1000MS

--- 2215-diff-two-arrs.rkt
+++ 2215-diff-two-arrs.rkt
@@ -1,9 +1,6 @@
 #lang racket
 
 (define (find-difference nums1 nums2)
-  (define n1set (list->set nums1))
-  (define n2set (list->set nums2))
-
-  (map set->list (list (set-subtract n1set n2set) (set-subtract n2set n1set))))
+  (map (compose set->list list->set) (list (remove* nums2 nums1) (remove* nums1 nums2))))
 
 (find-difference (list 1 2 3) (list 2 4 6))
