f3ae2e135d34bcfb30859e1f091383e38a099ba1
ktunprasert <ktunprasert@outlook.com>
2025-06-26 22:25:19 +0100

    2215: ok

--- 2215-diff-two-arrs.rkt
+++ 2215-diff-two-arrs.rkt
@@ -8,7 +8,7 @@
     (for/hash ([n2 nums2])
       (values n2 #t)))
 
-  (list (filter (curry (negate hash-has-key?) n2-map) nums1)
-        (filter (curry (negate hash-has-key?) n1-map) nums2)))
+  (list (filter (curry (negate hash-has-key?) n2-map) (hash-keys n1-map))
+        (filter (curry (negate hash-has-key?) n1-map) (hash-keys n2-map))))
 
 (find-difference (list 1 2 3) (list 2 4 6))
