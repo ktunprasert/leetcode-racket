9475f20294f950d1123e789411a9bc976c58a59b
ktunprasert <ktunprasert@outlook.com>
2025-06-26 23:27:10 +0100

    2778: length once

--- 2778.rkt
+++ 2778.rkt
@@ -1,7 +1,8 @@
 #lang racket
 
 (define (sum-of-squares nums)
-  (for/sum ([(n i) (in-indexed nums)] #:when (= 0 (modulo (length nums) (add1 i)))) (* n n)))
+  (define nlen (length nums))
+  (for/sum ([(n i) (in-indexed nums)] #:when (= 0 (modulo nlen (add1 i)))) (* n n)))
 
 ;; (sum-of-squares '[1 2 3 4])
 (sum-of-squares '[2 7 1 19 18 3])
