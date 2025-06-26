300acf9c7ec4b5f511eb5cc6c3b8bb8395c47f98
ktunprasert <ktunprasert@outlook.com>
2025-06-26 22:28:52 +0100

    2215: whoops slower

--- 2215-diff-two-arrs.rkt
+++ 2215-diff-two-arrs.rkt
@@ -4,6 +4,6 @@
   (define n1set (list->set nums1))
   (define n2set (list->set nums2))
 
-  (list (map set->list (list (set-subtract n1set n2set) (set-subtract n2set n1set)))))
+  (map set->list (list (set-subtract n1set n2set) (set-subtract n2set n1set))))
 
 (find-difference (list 1 2 3) (list 2 4 6))
