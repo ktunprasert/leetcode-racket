bc49f86c9837ab891976bed6b336483ee5a0a78a
ktunprasert <ktunprasert@outlook.com>
2025-06-26 22:28:25 +0100

    2215: use set

--- 2215-diff-two-arrs.rkt
+++ 2215-diff-two-arrs.rkt
@@ -1,14 +1,9 @@
 #lang racket
 
 (define (find-difference nums1 nums2)
-  (define n1-map
-    (for/hash ([n1 nums1])
-      (values n1 #t)))
-  (define n2-map
-    (for/hash ([n2 nums2])
-      (values n2 #t)))
+  (define n1set (list->set nums1))
+  (define n2set (list->set nums2))
 
-  (list (filter (curry (negate hash-has-key?) n2-map) (hash-keys n1-map))
-        (filter (curry (negate hash-has-key?) n1-map) (hash-keys n2-map))))
+  (list (map set->list (list (set-subtract n1set n2set) (set-subtract n2set n1set)))))
 
 (find-difference (list 1 2 3) (list 2 4 6))
