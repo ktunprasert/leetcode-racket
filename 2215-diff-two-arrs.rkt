e6e5225ac130d73039716174ce7e34c6fe07472f
ktunprasert <ktunprasert@outlook.com>
2025-06-26 22:51:35 +0100

    2215: null is faster?

--- 2215-diff-two-arrs.rkt
+++ 2215-diff-two-arrs.rkt
@@ -3,10 +3,10 @@
 (define (find-difference nums1 nums2)
   (define n1-map
     (for/hash ([n1 nums1])
-      (values n1 #t)))
+      (values n1 null)))
   (define n2-map
     (for/hash ([n2 nums2])
-      (values n2 #t)))
+      (values n2 null)))
 
   (list (filter-not (curry hash-has-key? n2-map) (hash-keys n1-map))
         (filter-not (curry hash-has-key? n1-map) (hash-keys n2-map))))
