7a34e5f85863925c7bdee46992ea42bae39a186f
ktunprasert <ktunprasert@outlook.com>
2025-06-26 22:50:44 +0100

    2215: cook deez

--- 2215-diff-two-arrs.rkt
+++ 2215-diff-two-arrs.rkt
@@ -1,6 +1,14 @@
 #lang racket
 
 (define (find-difference nums1 nums2)
-  (map (compose set->list list->set) (list (remove* nums2 nums1) (remove* nums1 nums2))))
+  (define n1-map
+    (for/hash ([n1 nums1])
+      (values n1 #t)))
+  (define n2-map
+    (for/hash ([n2 nums2])
+      (values n2 #t)))
+
+  (list (filter-not (curry hash-has-key? n2-map) (hash-keys n1-map))
+        (filter-not (curry hash-has-key? n1-map) (hash-keys n2-map))))
 
 (find-difference (list 1 2 3) (list 2 4 6))
