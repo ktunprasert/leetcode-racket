d93b4c81d947e3416d7596ca875964b4173a6583
ktunprasert <ktunprasert@outlook.com>
2025-06-29 16:37:12 +0100

    2697: for/lists

--- 2697.rkt
+++ 2697.rkt
@@ -2,21 +2,16 @@
 
 (define (make-smallest-palindrome s)
   (define slen (string-length s))
-  (let loop ([head empty]
-             [tail empty]
-             [i 0])
-    (cond
-      [(>= i (quotient slen 2))
-       (list->string (if (odd? slen)
-                         (append (reverse head) (cons (string-ref s (quotient slen 2)) tail))
-                         (append (reverse head) tail)))]
-      [((negate char=?) (string-ref s i) (string-ref s (- slen i 1)))
-       (let ([chead (string-ref s i)]
-             [ctail (string-ref s (- slen 1 i))])
-         (if (char<? chead ctail)
-             (loop (cons chead head) (cons chead tail) (add1 i))
-             (loop (cons ctail head) (cons ctail tail) (add1 i))))]
-      [else (loop (cons (string-ref s i) head) (cons (string-ref s (- slen 1 i)) tail) (add1 i))])))
+  (for*/lists (head tail
+                    #:result (if (even? slen)
+                                 (list->string (append (drop-right head 1) (cdr (reverse tail))))
+                                 (list->string (append head (cdr (reverse tail))))))
+              ([i (add1 (quotient slen 2))])
+              (define chead (string-ref s i))
+              (define ctail (string-ref s (- slen i 1)))
+              (if (char<? chead ctail)
+                  (values chead chead)
+                  (values ctail ctail))))
 
 (make-smallest-palindrome "efcge")
 (make-smallest-palindrome "abcd")
