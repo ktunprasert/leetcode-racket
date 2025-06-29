4f5d64f03110afbe3b3a308157b583358b27b922
ktunprasert <ktunprasert@outlook.com>
2025-06-29 16:14:03 +0100

    2427: ok ok

--- 2427-common-factors.rkt
+++ 2427-common-factors.rkt
@@ -1,9 +1,6 @@
 #lang racket
 
 (define (common-factors a b)
-  (for/sum ([i (in-inclusive-range 1 1000)] #:when (and (>= a i) (>= b i))
-                                            #:when (zero? (modulo a i))
-                                            #:when (zero? (modulo b i)))
-           1))
+  (count (λ (i) (and (zero? (modulo a i)) (zero? (modulo b i)))) (inclusive-range 1 (max a b))))
 
 (common-factors 12 6)
