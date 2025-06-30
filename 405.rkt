1c364fa5abababc501933d46be8e958b5eafb5bf
ktunprasert <ktunprasert@outlook.com>
2025-06-30 13:49:34 +0100

    405: ok ok

--- 405.rkt
+++ 405.rkt
@@ -5,19 +5,17 @@
         (if (negative? num)
             (+ (expt 2 32) num)
             num))
-
-  (define BBGGRR (build-list 6 (λ (n) (modulo (quotient num (expt 16 n)) 16))))
-
-  (define RRGGBB-char
-    (reverse (for/list ([n BBGGRR])
+  (define BGR (build-list 9 (λ (n) (modulo (quotient num (expt 16 n)) 16))))
+  (define RGB-char
+    (reverse (for/list ([n BGR])
                (cond
                  [(< n 10) (integer->char (+ n 48))]
                  [else (integer->char (+ 97 (- n 10)))]))))
-
-  (list->string (cond
-                  [(< num 256) (drop RRGGBB-char 4)]
-                  [(< num (* 256 256)) (drop RRGGBB-char 2)]
-                  [else RRGGBB-char])))
+  (define dropped (dropf RGB-char (curry char=? #\0)))
+  (if (zero? (length dropped))
+      "0"
+      (list->string dropped)))
 
 (to-hex 26)
 (to-hex -1)
+(to-hex 0)
