ffee6401c875e1ce25991645d7426f9bf658d3c7
ktunprasert <ktunprasert@outlook.com>
2025-06-30 23:52:28 +0100

    506: hmm

--- 506.rkt
+++ 506.rkt
@@ -3,14 +3,10 @@
 (define (find-relative-ranks score)
   (define score-map
     (for/hash ([n (sort score >)]
-               [i (in-naturals 1)])
-      (values n i)))
-  (for/list ([s score])
-    (match (hash-ref score-map s)
-      [1 "Gold Medal"]
-      [2 "Silver Medal"]
-      [3 "Bronze Medal"]
-      [n (~a n)])))
+               [i (sequence-append (list "Gold Medal" "Silver Medal" "Bronze Medal")
+                                   (in-naturals 4))])
+      (values n (~a i))))
+  (map (curry hash-ref score-map) score))
 
 (find-relative-ranks (list 5 4 3 2 1))
 (find-relative-ranks (list 10 3 8 9 4))
