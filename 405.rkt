36fa181949d009894c5bdf0a95badbba635b022b
ktunprasert <ktunprasert@outlook.com>
2025-06-30 13:44:57 +0100

    405: no std lib

--- 405.rkt
+++ 405.rkt
@@ -1,9 +1,23 @@
 #lang racket
 
 (define (to-hex num)
-  (if (negative? num)
-      (number->string (+ (expt 2 32) num) 16)
-      (number->string num 16)))
+  (set! num
+        (if (negative? num)
+            (+ (expt 2 32) num)
+            num))
+
+  (define BBGGRR (build-list 6 (λ (n) (modulo (quotient num (expt 16 n)) 16))))
+
+  (define RRGGBB-char
+    (reverse (for/list ([n BBGGRR])
+               (cond
+                 [(< n 10) (integer->char (+ n 48))]
+                 [else (integer->char (+ 97 (- n 10)))]))))
+
+  (list->string (cond
+                  [(< num 256) (drop RRGGBB-char 4)]
+                  [(< num (* 256 256)) (drop RRGGBB-char 2)]
+                  [else RRGGBB-char])))
 
 (to-hex 26)
 (to-hex -1)
