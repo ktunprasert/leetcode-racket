54430cba8b642fcf299973cb7f6b8ab3a8cdab1f
ktunprasert <ktunprasert@outlook.com>
2025-06-30 15:04:27 +0100

    495: ok

--- 495-teemo.rkt
+++ 495-teemo.rkt
@@ -2,15 +2,20 @@
 
 ;; (println (list total ns prev-end))
 
-(define (find-poisoned-duration timeSeries duration)
-  (+ duration
-     (for/sum ([t timeSeries] [a (drop timeSeries 1)])
-              (if (<= (- a t) duration)
-                  (- a t)
-                  (- a t 1)))))
+(define (find-poisoned-duration series duration)
+  (define tlen (length series))
+  (define time (* duration tlen))
+  (if (= 1 tlen)
+      time
+      (begin
+        (for ([a series]
+              [b (drop series 1)]
+              #:when (< b (+ a duration)))
+          (set! time (+ (- time duration) (- b a))))
+        time)))
 
 ;; (find-poisoned-duration (list 1 4) 2)
-;; (find-poisoned-duration (list 1 3) 3)
+(find-poisoned-duration (list 1 3) 3)
 ;; (find-poisoned-duration (list 1 2) 2)
 ;; (find-poisoned-duration (list 1 1) 2)
 ;; (find-poisoned-duration (list 1 1 2) 2)
@@ -18,4 +23,4 @@
 ;; (find-poisoned-duration (list 1 9) 10)
 ;; (find-poisoned-duration (list 1 10) 10)
 ;; (find-poisoned-duration (list 1 2 3 4 5 6 7 8 9 10) 10)
-(find-poisoned-duration (list 1 2 3 4 5 6 7 8 9) 1)
+;; (find-poisoned-duration (list 1 2 3 4 5 6 7 8 9) 1)
