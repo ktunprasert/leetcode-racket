e857f430067c7da639c9bc9c2a9b23afeb46f02e
ktunprasert <ktunprasert@outlook.com>
2025-06-30 14:29:40 +0100

    495: back to sets...

--- 495-teemo.rkt
+++ 495-teemo.rkt
@@ -1,19 +1,16 @@
 #lang racket
 
-(define (find-poisoned-duration timeSeries duration)
-  (let loop ([total 0]
-             [ns timeSeries]
-             [prev-end 0])
-    (cond
-      [(empty? ns) total]
-      [(= (car ns) prev-end) (loop (+ total (sub1 duration)) (cdr ns) (+ (car ns) (sub1 duration)))]
-      [(= (car ns) (sub1 prev-end))
-       (loop (+ total (- duration 2)) (cdr ns) (+ (car ns) (sub1 duration)))]
-      [(< (car ns) prev-end) (loop total (cdr ns) prev-end)]
-      [else (loop (+ duration total) (cdr ns) (+ (car ns) (sub1 duration)))])))
+;; (println (list total ns prev-end))
 
+(define (find-poisoned-duration timeSeries duration)
+  (set-count (for*/set ([t timeSeries]
+                        [t2 (in-range t (+ t duration))])
+               t2)))
+
+(find-poisoned-duration (list 1 3) 3)
 (find-poisoned-duration (list 1 4) 2)
-(find-poisoned-duration (list 1 2) 2)
-(find-poisoned-duration (list 1 1) 2)
-(find-poisoned-duration (list 1 1 2) 2)
+;; (find-poisoned-duration (list 1 2) 2)
+;; (find-poisoned-duration (list 1 1) 2)
+;; (find-poisoned-duration (list 1 1 2) 2)
 (find-poisoned-duration (list 1 2 3 4 5 6 7 8 9) 10)
+;; (find-poisoned-duration (list 1 2 3 4 5 6 7 8 9 10) 10)
