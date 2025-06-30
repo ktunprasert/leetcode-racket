7582807b3bae9f6bff58656f64e314c7a8d7c6bf
ktunprasert <ktunprasert@outlook.com>
2025-06-30 12:39:40 +0100

    389: not ok

--- 389-find-diff.rkt
+++ 389-find-diff.rkt
@@ -2,9 +2,10 @@
 
 (define/contract (find-the-difference s t)
   (-> string? string? char?)
-  (for/first ([c t]
-              #:unless (string-contains? s (~a c)))
-    c))
-
+  (or (for/first ([c t]
+                  #:unless (string-contains? s (~a c)))
+        c))
+  (string-ref t (sub1 (string-length t))))
 
 (find-the-difference "abcd" "abcde")
+(find-the-difference "aa" "aaa")
