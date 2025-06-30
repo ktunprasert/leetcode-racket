183509be1512f108d36f38f93c7a75854cc9741e
ktunprasert <ktunprasert@outlook.com>
2025-06-30 23:32:04 +0100

    507: ok

--- 507-perf-num.rkt
+++ 507-perf-num.rkt
@@ -1,7 +1,13 @@
 #lang racket
 
 (define (check-perfect-number num)
-  (= (for/sum ([n (in-range 1 num)] #:when (zero? (modulo num n))) n) num))
+  (if (= num 1)
+      false
+      (= (sub1 num)
+         (for/sum ([n (in-range 2 num)] #:break (> (* n n) num) #:when (zero? (modulo num n)))
+                  (+ n (quotient num n))))))
 
 (check-perfect-number 28)
 (check-perfect-number 7)
+(check-perfect-number 99999997)
+(check-perfect-number 1)
