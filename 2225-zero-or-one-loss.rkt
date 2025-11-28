c11f869b8d39fd13ebaf6e4c2a32773d4d8e053f
ktunprasert <ktunprasert@outlook.com>
2025-11-28 12:27:13 +0000

    2225: unwashed

---
+++ 2225-zero-or-one-loss.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+;; (define/contract (find-winners matches)
+;;   (-> (listof (listof exact-integer?)) (listof (listof exact-integer?)))
+;;   )
+
+(define (find-winners matches)
+  (define players (make-hash))
+  (for ([m matches])
+    (match-let ([(list winner loser) m])
+      (hash-update! players winner identity 0)
+      (hash-update! players loser add1 0)))
+
+  (for/fold ([zloss empty]
+             [oneloss empty]
+             #:result (list (reverse zloss) (reverse oneloss)))
+            ([p (in-hash-pairs players)])
+    (match-let ([(cons player score) p])
+
+      (match score
+        [0 (values (cons player zloss) oneloss)]
+        [1 (values zloss (cons player oneloss))]
+        [_ (values zloss oneloss)]))))
+
+(println (find-winners '((2 3) (1 3) (5 4) (6 4))))
