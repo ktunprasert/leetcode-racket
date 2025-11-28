266128789bfafd6e9f46fe011d05b7752e3bd638
ktunprasert <ktunprasert@outlook.com>
2025-11-28 12:39:38 +0000

    2225: 150mb to 130mb

--- 2225-zero-or-one-loss.rkt
+++ 2225-zero-or-one-loss.rkt
@@ -8,8 +8,9 @@
   (define players (make-hash))
   (for ([m matches])
     (match-let ([(list winner loser) m])
-      (hash-update! players winner identity 0)
-      (hash-update! players loser add1 0)))
+      (hash-ref! players winner 0)
+      (when (< (hash-ref! players loser 0) 2)
+        (hash-update! players loser add1 0))))
 
   (for/fold ([zloss empty]
              [oneloss empty]
@@ -21,4 +22,6 @@
         [1 (values zloss (cons player oneloss))]
         [_ (values zloss oneloss)]))))
 
+;; [[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]
+(println (find-winners '[[1 3] [2 3] [3 6] [5 6] [5 7] [4 5] [4 8] [4 9] [10 4] [10 9]]))
 (println (find-winners '((2 3) (1 3) (5 4) (6 4))))
